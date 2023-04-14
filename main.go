95e6e62e13c9451fd0b2e1298b2067049d07a1e1
Joshua Rich <joshua.rich@gmail.com>
2023-04-14 22:17:14 +1000

    Add check to ensure agent doesn't run as root or with elevated privileges.

--- main.go
+++ main.go
@@ -6,9 +6,27 @@
 package main
 
 import (
+	"log"
+	"syscall"
+
 	"github.com/joshuar/go-hass-agent/cmd"
 )
 
 func main() {
+	ensureNotEUID()
 	cmd.Execute()
+}
+
+// Following is copied from https://git.kernel.org/pub/scm/libs/libcap/libcap.git/tree/goapps/web/web.go
+//
+// ensureNotEUID aborts the program if it is running setuid something,
+// or being invoked by root.
+func ensureNotEUID() {
+	euid := syscall.Geteuid()
+	uid := syscall.Getuid()
+	egid := syscall.Getegid()
+	gid := syscall.Getgid()
+	if uid != euid || gid != egid || uid == 0 {
+		log.Fatalf("go-hass-agent should not be run with additional privileges or as root.")
+	}
 }
