3fa426950d02764d3d57bd3aef8ee7cff16cba9d
Joshua Rich <joshua.rich@gmail.com>
2023-04-25 09:57:11 +1000

    test(hass): test marshalling in api

--- internal/hass/api_test.go
+++ internal/hass/api_test.go
@@ -7,49 +7,126 @@ package hass
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
+	"reflect"
 	"testing"
 
-	"github.com/davecgh/go-spew/spew"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
-type FakeRequest struct {
-	fakeState string
+type mockRequest struct {
+	mock.Mock
+	requestType RequestType
+	data        interface{}
 }
 
-func (f *FakeRequest) RequestType() RequestType {
-	return RequestTypeUpdateSensorStates
+func (m *mockRequest) RequestType() RequestType {
+	m.On("RequestType")
+	m.Called()
+	return m.requestType
 }
 
-func (f *FakeRequest) RequestData() interface{} {
-	return struct {
-		State string `json:"State"`
-	}{
-		State: f.fakeState,
+func (m *mockRequest) RequestData() interface{} {
+	m.On("RequestData")
+	m.Called()
+	if m.data == nil {
+		return m.String()
+	} else {
+		return m.data
 	}
 }
 
-func (f *FakeRequest) ResponseHandler(b bytes.Buffer) {
-	spew.Dump(b.Bytes())
+func (m *mockRequest) ResponseHandler(b bytes.Buffer) {
+	m.On("ResponseHandler")
+	m.Called(b)
 }
 
-func TestMarshalJSONUnencrypted(t *testing.T) {
-	input := &FakeRequest{
-		fakeState: "foo",
-	}
-	output, _ := json.Marshal(&struct {
+var unencryptedRequest = &mockRequest{
+	requestType: RequestTypeUpdateSensorStates,
+}
+
+var encryptedRequest = &mockRequest{
+	requestType: RequestTypeEncrypted,
+}
+
+func TestMarshalJSON(t *testing.T) {
+	unencryptedRequest.data = ""
+	unencryptedRequestJSON, err := json.Marshal(&struct {
 		Type RequestType `json:"type"`
 		Data interface{} `json:"data"`
 	}{
 		Type: RequestTypeUpdateSensorStates,
-		Data: struct {
-			State string `json:"State"`
-		}{
-			State: input.fakeState,
-		},
+		Data: "",
 	})
-	b, _ := MarshalJSON(input, "")
-	if !bytes.Equal(b, output) {
-		t.Errorf("Expected %v but got %v", string(output), string(b))
+	assert.Nil(t, err)
+	encryptedRequest.data = ""
+	encryptedRequestJSON, err := json.Marshal(&struct {
+		Type          RequestType `json:"type"`
+		Encrypted     bool        `json:"encrypted"`
+		EncryptedData interface{} `json:"encrypted_data"`
+	}{
+		Type:          RequestTypeEncrypted,
+		Encrypted:     true,
+		EncryptedData: "",
+	})
+	assert.Nil(t, err)
+	type args struct {
+		request Request
+		secret  string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []byte
+		wantErr bool
+	}{
+		{
+			name: "unencrypted request",
+			args: args{request: unencryptedRequest},
+			want: unencryptedRequestJSON,
+		},
+		{
+			name:    "encrypted request without secret",
+			args:    args{request: encryptedRequest},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "encrypted request with secret",
+			args: args{request: encryptedRequest, secret: "fakeSecret"},
+			want: encryptedRequestJSON,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := MarshalJSON(tt.args.request, tt.args.secret)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("MarshalJSON() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("MarshalJSON() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestAPIRequest(t *testing.T) {
+	type args struct {
+		ctx     context.Context
+		request Request
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			APIRequest(tt.args.ctx, tt.args.request)
+		})
 	}
 }
