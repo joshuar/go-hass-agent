6b49890f7292843cfdb36eaa6dbdf4d63f25a041
Joshua Rich <joshua.rich@gmail.com>
2023-04-30 10:30:00 +1000

    refactor(linux): remove more unnecessary exports

--- internal/linux/network_connections_props_linux.go
+++ internal/linux/network_connections_props_linux.go
@@ -8,29 +8,29 @@ func _() {
 	// An "invalid array index" compiler error signifies that the constant values have changed.
 	// Re-run the stringer command to generate them again.
 	var x [1]struct{}
-	_ = x[ConnectionState-7]
-	_ = x[ConnectionID-8]
-	_ = x[ConnectionDevices-9]
-	_ = x[ConnectionType-10]
-	_ = x[ConnectionIPv4-11]
-	_ = x[ConnectionIPv6-12]
-	_ = x[AddressIPv4-13]
-	_ = x[AddressIPv6-14]
-	_ = x[WifiSSID-15]
-	_ = x[WifiFrequency-16]
-	_ = x[WifiSpeed-17]
-	_ = x[WifiStrength-18]
-	_ = x[WifiHWAddress-19]
+	_ = x[connectionState-2]
+	_ = x[connectionID-3]
+	_ = x[connectionDevices-4]
+	_ = x[connectionType-5]
+	_ = x[connectionIPv4-6]
+	_ = x[connectionIPv6-7]
+	_ = x[addressIPv4-8]
+	_ = x[addressIPv6-9]
+	_ = x[wifiSSID-10]
+	_ = x[wifiFrequency-11]
+	_ = x[wifiSpeed-12]
+	_ = x[wifiStrength-13]
+	_ = x[wifiHWAddress-14]
 }
 
-const _networkProp_name = "ConnectionStateConnectionIDConnectionDevicesConnectionTypeConnectionIPv4ConnectionIPv6AddressIPv4AddressIPv6WifiSSIDWifiFrequencyWifiSpeedWifiStrengthWifiHWAddress"
+const _networkProp_name = "connectionStateconnectionIDconnectionDevicesconnectionTypeconnectionIPv4connectionIPv6addressIPv4addressIPv6wifiSSIDwifiFrequencywifiSpeedwifiStrengthwifiHWAddress"
 
 var _networkProp_index = [...]uint8{0, 15, 27, 44, 58, 72, 86, 97, 108, 116, 129, 138, 150, 163}
 
 func (i networkProp) String() string {
-	i -= 7
+	i -= 2
 	if i < 0 || i >= networkProp(len(_networkProp_index)-1) {
-		return "networkProp(" + strconv.FormatInt(int64(i+7), 10) + ")"
+		return "networkProp(" + strconv.FormatInt(int64(i+2), 10) + ")"
 	}
 	return _networkProp_name[_networkProp_index[i]:_networkProp_index[i+1]]
 }
