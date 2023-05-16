d3d419a89763931a94ef7197b009925d085555fe
Joshua Rich <joshua.rich@gmail.com>
2023-05-16 16:27:36 +1000

    refactor(cmd): clean up logging/debugging/profiling startup

--- cmd/root.go
+++ cmd/root.go
@@ -6,14 +6,10 @@
 package cmd
 
 import (
-	"net/http"
 	_ "net/http/pprof"
-	"os"
 
 	"github.com/joshuar/go-hass-agent/internal/agent"
-	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
-	"github.com/rs/zerolog/pkgerrors"
 	"github.com/spf13/cobra"
 )
 
@@ -32,20 +28,9 @@ var rootCmd = &cobra.Command{
 	
 	It can also receive notifications from Home Assistant.`,
 	PersistentPreRun: func(cmd *cobra.Command, args []string) {
-		zerolog.ErrorStackMarshaler = pkgerrors.MarshalStack
-		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
-		if debugFlag {
-			zerolog.SetGlobalLevel(zerolog.DebugLevel)
-			log.Debug().Msg("Debug logging enabled.")
-		} else {
-			zerolog.SetGlobalLevel(zerolog.InfoLevel)
-		}
-		if profileFlag {
-			go func() {
-				log.Info().Err(http.ListenAndServe("localhost:6060", nil))
-			}()
-			log.Info().Msg("Profiling is enabled and available at localhost:6060.")
-		}
+		setLogging()
+		setDebugging()
+		setProfiling()
 	},
 	Run: func(cmd *cobra.Command, args []string) {
 		var runCmd func(string)
