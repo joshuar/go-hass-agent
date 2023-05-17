be8e51069c2afef3b8f79dbb513a2d1b1e012067
Joshua Rich <joshua.rich@gmail.com>
2023-05-17 10:43:27 +1000

    refactor(device): remove unused function

--- internal/device/device.go
+++ internal/device/device.go
@@ -26,19 +26,6 @@ type DeviceInfo interface {
 	AppData() interface{}
 }
 
-func GetDeviceInfo(d DeviceInfo) {
-	log.Info().Msgf("Device ID: %s", d.DeviceID())
-	log.Info().Msgf("Device Name: %s", d.DeviceName())
-	log.Info().Msgf("App ID: %s", d.AppID())
-	log.Info().Msgf("App Name: %s", d.AppName())
-	log.Info().Msgf("App Verson: %s", d.AppVersion())
-	log.Info().Msgf("Manufacturer: %s", d.Manufacturer())
-	log.Info().Msgf("Model: %s", d.Model())
-	log.Info().Msgf("OS Name: %s", d.OsName())
-	log.Info().Msgf("OS Version: %s", d.OsVersion())
-	log.Info().Msgf("Supports Encryption: %v", d.SupportsEncryption())
-}
-
 func GenerateRegistrationRequest(d DeviceInfo) *hass.RegistrationRequest {
 	if d.AppData() != nil {
 		return &hass.RegistrationRequest{
