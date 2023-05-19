6125e9faa8e51d7c6512adb603a4710b54434e0a
Joshua Rich <joshua.rich@gmail.com>
2023-05-20 09:37:27 +1000

    refactor(linux)!: remove deprecated functions and rework api struct
    This removes the functions for setting up DBus requests via the previous API access method.
    
    Also, the api struct has been reworked to avoid carrying around unnecessary variables.

--- internal/linux/api.go
+++ internal/linux/api.go
@@ -10,14 +10,23 @@ import (
 	"errors"
 )
 
-type DeviceAPI struct {
-	dBusSystem, dBusSession *bus
-	dbus                    map[string]*bus
-	Workers                 []func(context.Context, chan interface{})
+type LinuxDeviceAPI struct {
+	dbus map[string]*bus
 }
 
 // NewDeviceAPI sets up a DeviceAPI struct with appropriate DBus API endpoints
-func NewDeviceAPI(ctx context.Context) *DeviceAPI {
+func NewDeviceAPI(ctx context.Context) *LinuxDeviceAPI {
+
+	dbusEndpoints := make(map[string]*bus)
+	dbusEndpoints["session"] = newBus(ctx, sessionBus)
+	dbusEndpoints["system"] = newBus(ctx, systemBus)
+
+	return &LinuxDeviceAPI{
+		dbus: dbusEndpoints,
+	}
+}
+
+func (d *LinuxDeviceAPI) SensorWorkers() []func(context.Context, chan interface{}) {
 	var workers []func(context.Context, chan interface{})
 	workers = append(workers, LocationUpdater)
 	workers = append(workers, BatteryUpdater)
@@ -30,45 +39,11 @@ func NewDeviceAPI(ctx context.Context) *DeviceAPI {
 	workers = append(workers, LoadAvgUpdater)
 	workers = append(workers, DiskUsageUpdater)
 	workers = append(workers, TimeUpdater)
-
-	dbusEndpoints := make(map[string]*bus)
-	dbusEndpoints["session"] = newBus(ctx, sessionBus)
-	dbusEndpoints["system"] = newBus(ctx, systemBus)
-
-	api := &DeviceAPI{
-		dBusSystem:  dbusEndpoints["system"],
-		dBusSession: dbusEndpoints["session"],
-		dbus:        dbusEndpoints,	
-		Workers:     workers,
-	}
-	if api.dBusSystem == nil && api.dBusSession == nil {
-		return nil
-	} else {
-		// go api.monitorDBus(ctx)
-		return api
-	}
+	return workers
 }
 
-func (d *DeviceAPI) SensorWorkers() []func(context.Context, chan interface{}) {
-	return d.Workers
-}
-
-func (d *DeviceAPI) EndPoint(e string) interface{} {
+func (d *LinuxDeviceAPI) EndPoint(e string) interface{} {
 	return d.dbus[e]
-}
-
-// SessionBusRequest creates a request builder for the session bus
-func (d *DeviceAPI) SessionBusRequest() *busRequest {
-	return &busRequest{
-		bus: d.dBusSession,
-	}
-}
-
-// SystemBusRequest creates a request builder for the system bus
-func (d *DeviceAPI) SystemBusRequest() *busRequest {
-	return &busRequest{
-		bus: d.dBusSystem,
-	}
 }
 
 // key is an unexported type for keys defined in this package.
@@ -81,14 +56,14 @@ type key int
 var configKey key
 
 // StoreAPIInContext returns a new Context that embeds a DeviceAPI.
-func StoreAPIInContext(ctx context.Context, c *DeviceAPI) context.Context {
+func StoreAPIInContext(ctx context.Context, c *LinuxDeviceAPI) context.Context {
 	return context.WithValue(ctx, configKey, c)
 }
 
 // FetchAPIFromContext returns the DeviceAPI stored in ctx, or an error if there
 // is none
-func FetchAPIFromContext(ctx context.Context) (*DeviceAPI, error) {
-	if c, ok := ctx.Value(configKey).(*DeviceAPI); !ok {
+func FetchAPIFromContext(ctx context.Context) (*LinuxDeviceAPI, error) {
+	if c, ok := ctx.Value(configKey).(*LinuxDeviceAPI); !ok {
 		return nil, errors.New("no API in context")
 	} else {
 		return c, nil
