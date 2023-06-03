e1de4d5c9ce7a51f4c79f9e77a39504c7ba590b6
Joshua Rich <joshua.rich@gmail.com>
2023-06-03 13:54:15 +1000

    chore(hass): remove deprecated function

--- internal/hass/sensor.go
+++ internal/hass/sensor.go
@@ -5,8 +5,6 @@
 
 package hass
 
-import "encoding/json"
-
 // SensorUpdate represents an update for a sensor. It reflects the current state
 // of the sensor at the point in time it is used. It provides a bridge between
 // platform/device and HA implementations of what a sensor is.
@@ -104,58 +102,5 @@ func MarshalSensorRegistration(s Sensor) *sensorRegistrationInfo {
 		StateClass:        stateClass,
 		EntityCategory:    s.Category(),
 		Disabled:          s.Disabled(),
-	}
-}
-
-// MarshalSensorData takes the sensor data and returns the appropriate JSON
-// structure to either register or update the sensor in HA.
-func MarshalSensorData(s Sensor) *json.RawMessage {
-	var deviceClass, stateClass, sensorType string
-	if s.DeviceClass() == 0 {
-		deviceClass = ""
-	} else {
-		deviceClass = s.DeviceClass().String()
-	}
-	if s.StateClass() == 0 {
-		stateClass = ""
-	} else {
-		stateClass = s.StateClass().String()
-	}
-	if s.SensorType() == 0 {
-		sensorType = "sensor"
-	} else {
-		sensorType = s.SensorType().String()
-	}
-	var marshaled []byte
-	var raw json.RawMessage
-	if s.Registered() {
-		d := []sensorUpdateInfo{{
-			StateAttributes: s.Attributes(),
-			Icon:            s.Icon(),
-			State:           s.State(),
-			Type:            sensorType,
-			UniqueID:        s.ID(),
-		},
-		}
-		marshaled, _ = json.Marshal(d)
-		raw = json.RawMessage(marshaled)
-		return &raw
-	} else {
-		d := sensorRegistrationInfo{
-			StateAttributes:   s.Attributes(),
-			DeviceClass:       deviceClass,
-			Icon:              s.Icon(),
-			Name:              s.Name(),
-			State:             s.State(),
-			Type:              sensorType,
-			UniqueID:          s.ID(),
-			UnitOfMeasurement: s.Units(),
-			StateClass:        stateClass,
-			EntityCategory:    s.Category(),
-			Disabled:          s.Disabled(),
-		}
-		marshaled, _ = json.Marshal(d)
-		raw = json.RawMessage(marshaled)
-		return &raw
 	}
 }
