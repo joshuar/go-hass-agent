2e38d972c63a3a877cf16c0e2ccf083395c4f391
Joshua Rich <joshua.rich@gmail.com>
2023-07-04 00:47:19 +0000

    fix(agent,hass): store api and websocket urls in config
    This change moves the api and websocket URLs into the app config.
    Previously these were generated on app startup from other configuration
    values. This change stores them (after validation) on either
    registration or as part of a config upgrade, for retrieval on subsequent
    runs.

--- internal/hass/registration.go
+++ internal/hass/registration.go
@@ -7,12 +7,12 @@ package hass
 
 import (
 	"context"
+	"net/url"
 	"time"
 
 	"fyne.io/fyne/v2/data/binding"
 	"github.com/carlmjohnson/requests"
 	"github.com/go-playground/validator/v10"
-	"github.com/rs/zerolog/log"
 )
 
 type RegistrationDetails struct {
@@ -47,6 +47,40 @@ type RegistrationResponse struct {
 	WebhookID    string `json:"webhook_id"`
 }
 
+func (rr *RegistrationResponse) GenerateAPIURL(host string) string {
+	switch {
+	case rr.CloudhookURL != "":
+		return rr.CloudhookURL
+	case rr.RemoteUIURL != "" && rr.WebhookID != "":
+		return rr.RemoteUIURL + webHookPath + rr.WebhookID
+	case rr.WebhookID != "":
+		u, _ := url.Parse(host)
+		u = u.JoinPath(webHookPath, rr.WebhookID)
+		return u.String()
+	default:
+		return ""
+	}
+}
+
+func (rr *RegistrationResponse) GenerateWebsocketURL(host string) string {
+	// TODO: look into websocket http upgrade method
+	u, _ := url.Parse(host)
+	switch u.Scheme {
+	case "https":
+		u.Scheme = "wss"
+	case "http":
+		u.Scheme = "ws"
+	case "ws":
+		// nothing to do
+	case "wss":
+		// nothing to do
+	default:
+		u.Scheme = "ws"
+	}
+	u = u.JoinPath(websocketPath)
+	return u.String()
+}
+
 type RegistrationRequest struct {
 	AppData            interface{} `json:"app_data,omitempty"`
 	DeviceID           string      `json:"device_id"`
@@ -64,17 +98,21 @@ type RegistrationRequest struct {
 func RegisterWithHass(registration *RegistrationDetails) (*RegistrationResponse, error) {
 	request, err := registration.Device.MarshalJSON()
 	if err != nil {
-		log.Debug().Err(err).Msg("Unable to generate registration request.")
 		return nil, err
 	}
 	token, _ := registration.Token.Get()
 	host, _ := registration.Server.Get()
+	url, err := url.Parse(host)
+	if err != nil {
+		return nil, err
+	}
+	url = url.JoinPath("/api/mobile_app/registrations")
 
 	var response *RegistrationResponse
 	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
 	defer cancel()
 	err = requests.
-		URL(host+"/api/mobile_app/registrations").
+		URL(url.String()).
 		Header("Authorization", "Bearer "+token).
 		BodyBytes(request).
 		ToJSON(&response).
