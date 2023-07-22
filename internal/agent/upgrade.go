fc787aa92a6444553fef0752ef5ac689134fb657
Joshua Rich <joshua.rich@gmail.com>
2023-07-22 23:14:14 +0000

    refactor(agent): separate Upgrade function from being config exclusive

---
+++ internal/agent/upgrade.go
@@ -0,0 +1,51 @@
+// Copyright (c) 2023 Joshua Rich <joshua.rich@gmail.com>
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+package agent
+
+import (
+	"errors"
+	"time"
+
+	"github.com/rs/zerolog/log"
+	"golang.org/x/mod/semver"
+)
+
+func Upgrade(c *agentConfig) error {
+	configVersion := c.prefs.String("Version")
+	if configVersion == "" {
+		return errors.New("config version is not a valid value")
+	}
+
+	switch {
+	// * Upgrade host to include scheme for versions < v.1.4.0
+	case semver.Compare(configVersion, "v1.4.0") < 0:
+		log.Debug().Msg("Performing config upgrades for < v1.4.0")
+		hostString := c.prefs.String("Host")
+		if hostString == "" {
+			return errors.New("upgrade < v.1.4.0: invalid host value")
+		}
+		switch c.prefs.Bool("UseTLS") {
+		case true:
+			hostString = "https://" + hostString
+		case false:
+			hostString = "http://" + hostString
+		}
+		c.prefs.SetString("Host", hostString)
+		fallthrough
+	// * Add ApiURL and WebSocketURL config options for versions < v1.4.3
+	case semver.Compare(configVersion, "v1.4.3") < 0:
+		log.Debug().Msg("Performing config upgrades for < v1.4.3")
+		c.generateAPIURL()
+		c.generateWebsocketURL()
+	}
+
+	c.prefs.SetString("Version", Version)
+
+	// ! https://github.com/fyne-io/fyne/issues/3170
+	time.Sleep(110 * time.Millisecond)
+
+	return nil
+}
