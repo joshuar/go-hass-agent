8243f670a30284de51e3a969f7641f953cb39afc
Joshua Rich <joshua.rich@gmail.com>
2023-08-27 01:39:40 +0000

    fix(linux): add data source to kernel, distribution version/name sensors

--- internal/linux/versionSensor.go
+++ internal/linux/versionSensor.go
@@ -25,17 +25,20 @@ func Versions(ctx context.Context, status chan interface{}) {
 		value:      info.KernelVersion,
 		diagnostic: true,
 		icon:       "mdi:chip",
+		source:     "procfs",
 	}
 	status <- &linuxSensor{
 		sensorType: distribution,
 		value:      cases.Title(language.English).String(info.Platform),
 		diagnostic: true,
 		icon:       "mdi:linux",
+		source:     "procfs",
 	}
 	status <- &linuxSensor{
 		sensorType: version,
 		value:      info.PlatformVersion,
 		diagnostic: true,
 		icon:       "mdi:numeric",
+		source:     "procfs",
 	}
 }
