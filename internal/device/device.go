5f942c760e792c1b38e5836672eb83c4402e0f69
Joshua Rich <joshua.rich@gmail.com>
2023-09-25 06:04:03 +0000

    refactor(tracker,device): move StartWorkers out of tracker and into device package

--- internal/device/device.go
+++ internal/device/device.go
@@ -5,7 +5,10 @@
 
 package device
 
-import "context"
+import (
+	"context"
+	"sync"
+)
 
 // SensorTracker is the interface through which a device can update its sensors.
 type SensorTracker interface {
@@ -13,4 +16,18 @@ type SensorTracker interface {
 	// the sensor tracker, which will handle updating its internal database and
 	// Home Assistant.
 	UpdateSensors(context.Context, ...interface{}) error
+}
+
+// StartWorkers will call all the sensor worker functions that have been defined
+// for this device.
+func StartWorkers(ctx context.Context, workers []func(context.Context, SensorTracker), tracker SensorTracker) {
+	var wg sync.WaitGroup
+	for _, worker := range workers {
+		wg.Add(1)
+		go func(worker func(context.Context, SensorTracker)) {
+			defer wg.Done()
+			worker(ctx, tracker)
+		}(worker)
+	}
+	wg.Wait()
 }
