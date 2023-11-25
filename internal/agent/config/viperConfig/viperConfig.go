8ae9fe885de00e6365d2a076127552f3d8f912b9
Joshua Rich <joshua.rich@gmail.com>
2023-11-25 14:36:18 +1000

    style(agent): document ViperConfig class

--- internal/agent/config/viperConfig/viperConfig.go
+++ internal/agent/config/viperConfig/viperConfig.go
@@ -26,6 +26,10 @@ type ViperConfig struct {
 	path  string
 }
 
+// Get sets the passed in value parameter to the value of the passed in key. The
+// passed in value needs to be one of the supported values, either a string or
+// bool reference. If the key is not set or the type of the passed in value is
+// unsupported, the returned error will be non-nil.
 func (c *ViperConfig) Get(key string, value interface{}) error {
 	switch v := value.(type) {
 	case *string:
@@ -41,6 +45,9 @@ func (c *ViperConfig) Get(key string, value interface{}) error {
 	return nil
 }
 
+// Set will assign the passed in value to the passed in key in the config. If
+// there is a problem setting the value or the key does not exist, it will
+// return a non-nil error.
 func (c *ViperConfig) Set(key string, value interface{}) error {
 	c.store.Set(key, value)
 	if err := c.store.WriteConfigAs(filepath.Join(c.path, configFile)); err != nil {
@@ -52,14 +59,20 @@ func (c *ViperConfig) Set(key string, value interface{}) error {
 	return nil
 }
 
+// Delete is currently unimplemented for ViperConfig.
 func (c *ViperConfig) Delete(key string) error {
 	return nil
 }
 
+// StoragePath returns a full path on the filesystem whose trailing path will be
+// the given id.
 func (c *ViperConfig) StoragePath(id string) (string, error) {
 	return filepath.Join(c.path, id), nil
 }
 
+// New sets up a new ViperConfig object at the given path. If there is already a
+// config at the path, it is opened and returned. Otherwise a new config is
+// initialised.
 func New(path string) (*ViperConfig, error) {
 	c := &ViperConfig{
 		store: viper.New(),
