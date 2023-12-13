083e034de6f98d11d905e8aebb9080ab7e66fbc0
Joshua Rich <joshua.rich@gmail.com>
2023-12-13 10:42:09 +1000

    test(dbushelpers): add test for VariantToValue function

--- pkg/dbushelpers/dbus_test.go
+++ pkg/dbushelpers/dbus_test.go
@@ -735,22 +735,27 @@ func TestGetSessionPath(t *testing.T) {
 	}
 }
 
-// func TestVariantToValue(t *testing.T) {
-// 	type args struct {
-// 		variant dbus.Variant
-// 	}
-// 	tests := []struct {
-// 		name string
-// 		args args
-// 		want S
-// 	}{
-// 		// TODO: Add test cases.
-// 	}
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			if got := VariantToValue(tt.args.variant); !reflect.DeepEqual(got, tt.want) {
-// 				t.Errorf("VariantToValue() = %v, want %v", got, tt.want)
-// 			}
-// 		})
-// 	}
-// }
+func TestVariantToValue(t *testing.T) {
+	type args struct {
+		variant dbus.Variant
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "string conversion",
+			args: args{variant: dbus.MakeVariant("foo")},
+			want: "foo",
+		},
+		// TODO: Test all possible variant values?
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := VariantToValue[string](tt.args.variant); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("VariantToValue() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
