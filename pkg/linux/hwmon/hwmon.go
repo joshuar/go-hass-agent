b0e7331665b9a38ed5e5e454333b9d255bf9b2a2
Joshua Rich <joshua.rich@gmail.com>
2024-01-21 20:46:51 +1000

    fix(linux/hwmon): :sparkles: expose alarm and intrusion as separate sensors

--- pkg/linux/hwmon/hwmon.go
+++ pkg/linux/hwmon/hwmon.go
@@ -13,6 +13,9 @@ import (
 	"strconv"
 	"strings"
 	"sync"
+
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
 )
 
 //go:generate stringer -type=SensorType -output sensorType_strings.go
@@ -28,6 +31,8 @@ const (
 	Energy
 	Humidity
 	Frequency
+	Alarm
+	Intrusion
 )
 
 type SensorType int
@@ -63,6 +68,10 @@ func (s *Sensor) Value() float64 {
 // Name returns a name for the sensor. It will be derived from the chip name
 // plus either any label, else name of the sensor itself.
 func (s *Sensor) Name() string {
+	if s.SensorType == Alarm {
+		c := cases.Title(language.AmericanEnglish)
+		return c.String(s.chip + " " + s.id + " " + s.label)
+	}
 	if s.label != "" {
 		return s.chip + " " + s.label
 	}
@@ -77,7 +86,7 @@ func (s *Sensor) Units() string {
 // String will format the sensor name and value as a pretty string.
 func (s *Sensor) String() string {
 	var b strings.Builder
-	fmt.Fprintf(&b, "%s: %.2f %s [%s]", s.Name(), s.Value(), s.Units(), s.SensorType)
+	fmt.Fprintf(&b, "%s: %.3f %s [%s]", s.Name(), s.Value(), s.Units(), s.SensorType)
 	for i, a := range s.Attributes {
 		if i == 0 {
 			fmt.Fprintf(&b, " (")
@@ -94,15 +103,22 @@ func (s *Sensor) String() string {
 }
 
 func (s *Sensor) update(d details) error {
-	switch d.item {
-	case "input":
+	switch {
+	case d.item == "label":
+		s.label = d.value
+	case d.item == "input":
 		v, err := strconv.ParseFloat(d.value, 64)
 		if err != nil {
 			return err
 		}
 		s.value = v
-	case "label":
-		s.label = d.value
+	case strings.Contains(d.item, "alarm"):
+		v, err := strconv.ParseFloat(d.value, 64)
+		if err != nil {
+			return err
+		}
+		s.label = strings.ReplaceAll(d.item, "_", " ")
+		s.value = v
 	default:
 		v, err := strconv.ParseFloat(d.value, 64)
 		if err != nil {
@@ -113,15 +129,16 @@ func (s *Sensor) update(d details) error {
 	return nil
 }
 
-func newSensor(chip, name string) *Sensor {
-	t, f, u := parseType(name)
-	return &Sensor{
+func newSensor(chip string, details *details) *Sensor {
+	t, f, u := parseType(details)
+	s := &Sensor{
 		chip:        chip,
-		id:          name,
+		id:          details.id,
 		SensorType:  t,
 		scaleFactor: f,
 		units:       u,
 	}
+	return s
 }
 
 // Attribute represents an attribute of a sensor, like its max, min or average
@@ -133,7 +150,7 @@ type Attribute struct {
 
 // String will format the attribute name and value as a pretty string.
 func (a *Attribute) String() string {
-	return fmt.Sprintf("%s: %.2f", a.Name, a.Value)
+	return fmt.Sprintf("%s: %.3f", a.Name, a.Value)
 }
 
 type details struct {
@@ -163,25 +180,29 @@ func getValue(p string) (string, error) {
 	return strings.TrimSpace(string(b)), nil
 }
 
-func parseType(n string) (sensorType SensorType, scaleFactor float64, units string) {
+func parseType(d *details) (sensorType SensorType, scaleFactor float64, units string) {
 	switch {
-	case strings.Contains(n, "temp"):
+	case strings.Contains(d.item, "intrusion"):
+		return Intrusion, 1, ""
+	case strings.Contains(d.item, "alarm"):
+		return Alarm, 1, ""
+	case strings.Contains(d.id, "temp"):
 		return Temp, 1000, "°C"
-	case strings.Contains(n, "fan"):
+	case strings.Contains(d.id, "fan"):
 		return Fan, 1, "rpm"
-	case strings.Contains(n, "in"):
+	case strings.Contains(d.id, "in"):
 		return Voltage, 1000, "V"
-	case strings.Contains(n, "pwm"):
+	case strings.Contains(d.id, "pwm"):
 		return PWM, 1, "Hz"
-	case strings.Contains(n, "curr"):
+	case strings.Contains(d.id, "curr"):
 		return Current, 1, "A"
-	case strings.Contains(n, "power"):
+	case strings.Contains(d.id, "power"):
 		return Power, 1000, "W"
-	case strings.Contains(n, "energy"):
+	case strings.Contains(d.id, "energy"):
 		return Energy, 1000, "J"
-	case strings.Contains(n, "humidity"):
+	case strings.Contains(d.id, "humidity"):
 		return Humidity, 1, "%"
-	case strings.Contains(n, "freq"):
+	case strings.Contains(d.id, "freq"):
 		return Frequency, 1000, "Hz"
 	default:
 		return Unknown, 1, ""
@@ -216,11 +237,13 @@ func processSensors(path string) (sensorCh <-chan Sensor, errCh <-chan error) {
 	go func() {
 		defer wg.Done()
 		for d := range dc {
+			s := newSensor(namePrefix, &d)
+			id := s.SensorType.String() + "_" + s.id
 			mu.Lock()
-			if _, ok := smap[d.id]; !ok {
-				smap[d.id] = newSensor(namePrefix, d.id)
+			if _, ok := smap[id]; !ok {
+				smap[id] = s
 			}
-			if err := smap[d.id].update(d); err != nil {
+			if err := smap[id].update(d); err != nil {
 				errc <- err
 			}
 			mu.Unlock()
