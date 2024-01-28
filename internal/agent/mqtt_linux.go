b094c4a081db82c628e72097f8bacc4c039ffa50
Joshua Rich <joshua.rich@gmail.com>
2024-01-28 15:16:17 +1000

    feat(agent): :art: MQTT agent adjustments
    add Power Off and Reboot controls; alter device info presented to Home Assistant to match registration info; simplify code and renaming; update documentation

--- internal/agent/mqtt_linux.go
+++ internal/agent/mqtt_linux.go
@@ -9,59 +9,88 @@ import (
 	"context"
 
 	MQTT "github.com/eclipse/paho.mqtt.golang"
+	"github.com/godbus/dbus/v5"
 	"github.com/rs/zerolog/log"
 
 	mqtthass "github.com/joshuar/go-hass-anything/v3/pkg/hass"
 
 	"github.com/joshuar/go-hass-agent/internal/agent/config"
+	"github.com/joshuar/go-hass-agent/internal/linux"
 	"github.com/joshuar/go-hass-agent/pkg/linux/dbusx"
 )
 
-func (a *Agent) newMQTTDevice(ctx context.Context) *mqttDevice {
-	cfg := config.FetchFromContext(ctx)
+const (
+	dbusDest           = "org.freedesktop.login1"
+	dbusLockMethod     = dbusDest + ".Session.Lock"
+	dbusUnlockMethod   = dbusDest + ".Session.UnLock"
+	dbusRebootMethod   = dbusDest + ".Manager.Reboot"
+	dbusPowerOffMethod = dbusDest + ".Manager.PowerOff"
+)
 
-	var deviceName string
-	cfg.Get(config.PrefDeviceName, &deviceName)
-	deviceInfo := &mqtthass.Device{
-		Name:         deviceName,
-		URL:          "https://github.com/joshuar/go-hass-agent",
-		SWVersion:    config.AppVersion,
-		Manufacturer: "go-hass-agent",
-		Model:        a.AppID(),
-		Identifiers:  []string{"go-hass-agent01"},
+func newMQTTObject(ctx context.Context) *mqttObj {
+	appName := "go_hass_agent"
+
+	baseEntity := func(entityID string) *mqtthass.EntityConfig {
+		return mqtthass.NewEntityByID(entityID, appName).
+			AsButton().
+			WithDefaultOriginInfo().
+			WithDeviceInfo(mqttDevice())
+	}
+
+	dbusCall := func(ctx context.Context, path dbus.ObjectPath, dest, method string, args ...any) error {
+		return dbusx.NewBusRequest(ctx, dbusx.SystemBus).
+			Path(path).
+			Destination(dest).
+			Call(method, args...)
 	}
 
 	sessionPath := dbusx.GetSessionPath(ctx)
-	configs := make(map[string]*mqtthass.EntityConfig)
-	configs["lock_session"] = mqtthass.NewEntityByID("lock_session", "go_hass_agent").
-		AsButton().
-		WithDefaultOriginInfo().
-		WithDeviceInfo(deviceInfo).
+	entities := make(map[string]*mqtthass.EntityConfig)
+	entities["lock_session"] = baseEntity("lock_session").
 		WithIcon("mdi:eye-lock").
 		WithCommandCallback(func(_ MQTT.Client, _ MQTT.Message) {
-			err := dbusx.NewBusRequest(ctx, dbusx.SystemBus).
-				Path(sessionPath).
-				Destination("org.freedesktop.login1").
-				Call("org.freedesktop.login1.Session.Lock")
+			err := dbusCall(ctx, sessionPath, dbusDest, dbusLockMethod)
 			if err != nil {
 				log.Warn().Err(err).Msg("Could not lock session.")
 			}
 		})
-	configs["unlock_session"] = mqtthass.NewEntityByID("unlock_session", "go_hass_agent").
-		AsButton().
-		WithDefaultOriginInfo().
-		WithDeviceInfo(deviceInfo).
+	entities["unlock_session"] = baseEntity("unlock_session").
 		WithIcon("mdi:eye-lock-open").
 		WithCommandCallback(func(_ MQTT.Client, _ MQTT.Message) {
-			err := dbusx.NewBusRequest(ctx, dbusx.SystemBus).
-				Path(sessionPath).
-				Destination("org.freedesktop.login1").
-				Call("org.freedesktop.login1.Session.UnLock")
+			err := dbusCall(ctx, sessionPath, dbusDest, dbusUnlockMethod)
 			if err != nil {
 				log.Warn().Err(err).Msg("Could not unlock session.")
 			}
 		})
-	return &mqttDevice{
-		configs: configs,
+	entities["reboot"] = baseEntity("reboot").
+		WithIcon("mdi:restart").
+		WithCommandCallback(func(_ MQTT.Client, _ MQTT.Message) {
+			err := dbusCall(ctx, dbus.ObjectPath("/org/freedesktop/login1"), dbusDest, dbusRebootMethod, true)
+			if err != nil {
+				log.Warn().Err(err).Msg("Could not reboot session.")
+			}
+		})
+	entities["poweroff"] = baseEntity("poweroff").
+		WithIcon("mdi:power").
+		WithCommandCallback(func(_ MQTT.Client, _ MQTT.Message) {
+			err := dbusCall(ctx, dbus.ObjectPath("/org/freedesktop/login1"), dbusDest, dbusPowerOffMethod, true)
+			if err != nil {
+				log.Warn().Err(err).Msg("Could not power off session.")
+			}
+		})
+	return &mqttObj{
+		entities: entities,
+	}
+}
+
+func mqttDevice() *mqtthass.Device {
+	dev := linux.NewDevice(config.AppName, config.AppVersion)
+	return &mqtthass.Device{
+		Name:         dev.DeviceName(),
+		URL:          config.AppURL,
+		SWVersion:    dev.OsVersion(),
+		Manufacturer: dev.Manufacturer(),
+		Model:        dev.Model(),
+		Identifiers:  []string{dev.DeviceID()},
 	}
 }
