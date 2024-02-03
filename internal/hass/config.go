95db2df9fe794b4bd45563da22eee6fea2aaf2c9
Joshua Rich <joshua.rich@gmail.com>
2024-02-03 16:03:50 +1000

    refactor(hass): :recycle: clean up Home Assistant config code

--- internal/hass/config.go
+++ internal/hass/config.go
@@ -16,78 +16,52 @@ import (
 	"github.com/joshuar/go-hass-agent/internal/hass/api"
 )
 
-type haConfig struct {
-	rawConfigProps map[string]any
-	haConfigProps
+type config struct {
+	rawProperties map[string]any
+	properties
 	mu sync.Mutex
 }
 
-type haConfigProps struct {
-	Entities   map[string]map[string]any `json:"entities"`
-	UnitSystem struct {
-		Length      string `json:"length"`
-		Mass        string `json:"mass"`
-		Temperature string `json:"temperature"`
-		Volume      string `json:"volume"`
-	} `json:"unit_system"`
-	ConfigDir             string   `json:"config_dir"`
-	LocationName          string   `json:"location_name"`
-	TimeZone              string   `json:"time_zone"`
-	Version               string   `json:"version"`
-	Components            []string `json:"components"`
-	WhitelistExternalDirs []string `json:"whitelist_external_dirs"`
-	Elevation             int      `json:"elevation"`
-	Latitude              float64  `json:"latitude"`
-	Longitude             float64  `json:"longitude"`
+type properties struct {
+	Entities              map[string]map[string]any `json:"entities"`
+	UnitSystem            units                     `json:"unit_system"`
+	ConfigDir             string                    `json:"config_dir"`
+	LocationName          string                    `json:"location_name"`
+	TimeZone              string                    `json:"time_zone"`
+	Version               string                    `json:"version"`
+	Components            []string                  `json:"components"`
+	WhitelistExternalDirs []string                  `json:"whitelist_external_dirs"`
+	Elevation             int                       `json:"elevation"`
+	Latitude              float64                   `json:"latitude"`
+	Longitude             float64                   `json:"longitude"`
+}
+
+type units struct {
+	Length      string `json:"length"`
+	Mass        string `json:"mass"`
+	Temperature string `json:"temperature"`
+	Volume      string `json:"volume"`
 }
 
 // HassConfig implements hass.Request so that it can be sent as a request to HA
 // to get its data.
 
-func (h *haConfig) RequestType() api.RequestType {
+func (c *config) RequestType() api.RequestType {
 	return api.RequestTypeGetConfig
 }
 
-func (h *haConfig) RequestData() json.RawMessage {
+func (c *config) RequestData() json.RawMessage {
 	return nil
 }
 
-func (h *haConfig) extractConfig(b []byte) {
-	if b == nil {
-		log.Warn().Msg("No config returned.")
-		return
-	}
-	h.mu.Lock()
-	result, err := marshmallow.Unmarshal(b, &h.haConfigProps)
-	if err != nil {
-		log.Warn().Msg("Could not extract config structure.")
-	}
-	h.rawConfigProps = result
-	h.mu.Unlock()
+func (c *config) GetRegisteredEntities() map[string]map[string]any {
+	return c.Entities
 }
 
-func GetHassConfig(ctx context.Context) (*haConfig, error) {
-	h := new(haConfig)
-	response := <-api.ExecuteRequest(ctx, h)
-	switch r := response.(type) {
-	case []byte:
-		h.extractConfig(response.([]byte))
-	case error:
-		log.Warn().Err(r).Msg("Failed to fetch Home Assistant config.")
-	default:
-		log.Warn().Msgf("Unknown response type %T", r)
-	}
-	return h, nil
-}
-
-func (h *haConfig) GetRegisteredEntities() map[string]map[string]any {
-	return h.Entities
-}
-
-func (h *haConfig) IsEntityDisabled(entity string) (bool, error) {
-	h.mu.Lock()
-	defer h.mu.Unlock()
-	if v, ok := h.Entities[entity]["disabled"]; ok {
+func (c *config) IsEntityDisabled(entity string) (bool, error) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if v, ok := c.Entities[entity]["disabled"]; ok {
 		if disabledState, ok := v.(bool); !ok {
 			return false, nil
 		} else {
@@ -97,6 +71,34 @@ func (h *haConfig) IsEntityDisabled(entity string) (bool, error) {
 	return false, nil
 }
 
-func (h *haConfig) GetVersion() string {
-	return h.Version
+func (c *config) GetVersion() string {
+	return c.Version
+}
+
+func (c *config) extractConfig(b []byte) {
+	if b == nil {
+		log.Warn().Msg("No config returned.")
+		return
+	}
+	c.mu.Lock()
+	result, err := marshmallow.Unmarshal(b, &c.properties)
+	if err != nil {
+		log.Warn().Msg("Could not extract config structure.")
+	}
+	c.rawProperties = result
+	c.mu.Unlock()
+}
+
+func GetConfig(ctx context.Context) (*config, error) {
+	h := new(config)
+	response := <-api.ExecuteRequest(ctx, h)
+	switch r := response.(type) {
+	case []byte:
+		h.extractConfig(r)
+	case error:
+		log.Warn().Err(r).Msg("Failed to fetch Home Assistant config.")
+	default:
+		log.Warn().Msgf("Unknown response type %T", r)
+	}
+	return h, nil
 }
