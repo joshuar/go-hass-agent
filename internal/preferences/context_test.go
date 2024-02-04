d106b0c76769bc0ea335f68992d9449a9d997296
Joshua Rich <joshua.rich@gmail.com>
2024-02-04 11:02:50 +1000

    test(preferences): :white_check_mark: add tests for preferences package

---
+++ internal/preferences/context_test.go
@@ -0,0 +1,68 @@
+// Copyright (c) 2024 Joshua Rich <joshua.rich@gmail.com>
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+package preferences
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEmbedInContext(t *testing.T) {
+	type args struct {
+		ctx context.Context
+		p   *Preferences
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "default",
+			args: args{ctx: context.TODO(), p: defaultPreferences()},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := EmbedInContext(tt.args.ctx, tt.args.p)
+			_, ok := got.Value(cfgKey).(Preferences)
+			assert.True(t, ok)
+		})
+	}
+}
+
+func TestFetchFromContext(t *testing.T) {
+	prefs := &Preferences{
+		DeviceName: "testDevice",
+	}
+	fullCtx := EmbedInContext(context.TODO(), prefs)
+
+	type args struct {
+		ctx context.Context
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "fullCtx",
+			args: args{ctx: fullCtx},
+			want: "testDevice",
+		},
+		{
+			name: "emptyCtx",
+			args: args{ctx: context.TODO()},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := FetchFromContext(tt.args.ctx)
+			assert.Equal(t, got.DeviceName, tt.want)
+		})
+	}
+}
