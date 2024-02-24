324c69bcc216ebe2c1f5f4f59ece86d9152c503f
Joshua Rich <joshua.rich@gmail.com>
2024-02-24 14:27:10 +1000

    test(hass): :white_check_mark: add tests for hass context

---
+++ internal/hass/context_test.go
@@ -0,0 +1,163 @@
+// Copyright (c) 2024 Joshua Rich <joshua.rich@gmail.com>
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+package hass
+
+import (
+	"context"
+	"reflect"
+	"testing"
+
+	"github.com/go-resty/resty/v2"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/joshuar/go-hass-agent/internal/preferences"
+)
+
+func TestContextSetURL(t *testing.T) {
+	type args struct {
+		ctx context.Context
+		url string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "successful test",
+			args: args{ctx: context.TODO(), url: "good"},
+			want: "good",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := ContextSetURL(tt.args.ctx, tt.args.url)
+			url, ok := got.Value(urlContextKey).(string)
+			assert.True(t, ok)
+			assert.Equal(t, url, tt.want)
+		})
+	}
+}
+
+func TestContextGetURL(t *testing.T) {
+	testCtx := ContextSetURL(context.TODO(), "test")
+
+	type args struct {
+		ctx context.Context
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "successful test",
+			args: args{ctx: testCtx},
+			want: "test",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ContextGetURL(tt.args.ctx); got != tt.want {
+				t.Errorf("ContextGetURL() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestContextSetClient(t *testing.T) {
+	goodClient := resty.New()
+	type args struct {
+		ctx    context.Context
+		client *resty.Client
+	}
+	tests := []struct {
+		name string
+		args args
+		want *resty.Client
+	}{
+		{
+			name: "successful test",
+			args: args{ctx: context.TODO(), client: goodClient},
+			want: goodClient,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := ContextSetClient(tt.args.ctx, tt.args.client)
+			client, ok := got.Value(clientContextKey).(*resty.Client)
+			assert.True(t, ok)
+			if !reflect.DeepEqual(client, tt.want) {
+				t.Errorf("ContextSetClient() = %v, want %v", client, tt.want)
+			}
+		})
+	}
+}
+
+func TestContextGetClient(t *testing.T) {
+	goodClient := resty.New()
+	goodCtx := ContextSetClient(context.TODO(), goodClient)
+	type args struct {
+		ctx context.Context
+	}
+	tests := []struct {
+		name string
+		args args
+		want *resty.Client
+	}{
+		{
+			name: "successful test",
+			args: args{ctx: goodCtx},
+			want: goodClient,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ContextGetClient(tt.args.ctx); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ContextGetClient() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestNewContext(t *testing.T) {
+	preferences.SetPath(t.TempDir())
+
+	testServer := "http://test.host:9999"
+
+	preferences.Save(
+		preferences.Host(testServer),
+		preferences.Token("testToken"),
+		preferences.CloudhookURL(""),
+		preferences.RemoteUIURL(""),
+		preferences.WebhookID("testID"),
+		preferences.Secret(""),
+		preferences.RestAPIURL(testServer),
+		preferences.WebsocketURL(testServer),
+		preferences.DeviceName("testDevice"),
+		preferences.DeviceID("testID"),
+		preferences.Version("6.4.0"),
+		preferences.Registered(true),
+	)
+	tests := []struct {
+		name    string
+		wantURL string
+	}{
+		{
+			name:    "successful test",
+			wantURL: testServer,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, _ := NewContext()
+			gotURL := ContextGetURL(got)
+			assert.Equal(t, gotURL, tt.wantURL)
+			gotClient := ContextGetClient(got)
+			assert.NotNil(t, gotClient)
+		})
+	}
+}
