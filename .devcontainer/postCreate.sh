00c593ecd22712d678d48b5a7b95df7d7a6609e0
Joshua Rich <joshua.rich@gmail.com>
2024-04-25 11:31:10 +1000

    build(devcontainer): :recycle: update devcontainer environment

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -4,12 +4,13 @@ set -e
 
 cd /workspaces/go-hass-agent && go mod tidy
 
-# Install goreleaser 
-go install github.com/goreleaser/goreleaser@latest
-
 # Install go build packages
 go install golang.org/x/tools/cmd/stringer@latest
 go install github.com/fyne-io/fyne-cross@latest
 go install golang.org/x/text/cmd/gotext@latest
 go install github.com/matryer/moq@latest
 go install github.com/spf13/cobra-cli@latest
+go install github.com/goreleaser/nfpm/v2/cmd/nfpm@latest
+go install github.com/tomwright/dasel/v2/cmd/dasel@latest
+
+exit 0
