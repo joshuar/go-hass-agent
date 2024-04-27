1fd50fb7c6a461efe9deb6e8e1cb2a5878f4686b
Joshua Rich <joshua.rich@gmail.com>
2024-04-27 17:06:03 +1000

    build(devcontainer): :sparkles: ensure cross-libs for arm/arm64 are installed

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -9,8 +9,8 @@ go install golang.org/x/tools/cmd/stringer@latest
 go install github.com/fyne-io/fyne-cross@latest
 go install golang.org/x/text/cmd/gotext@latest
 go install github.com/matryer/moq@latest
-go install github.com/spf13/cobra-cli@latest
 go install github.com/goreleaser/nfpm/v2/cmd/nfpm@latest
 go install github.com/tomwright/dasel/v2/cmd/dasel@latest
+go install github.com/sigstore/cosign/v2/cmd/cosign@latest
 
 exit 0
