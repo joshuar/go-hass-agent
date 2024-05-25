daf0ffd4851039c6a37d5cbde14d065bdca65f51
Joshua Rich <joshua.rich@gmail.com>
2024-05-25 22:24:28 +1000

    build(go): :arrow_up: update tooling dependencies

--- tools/tools.go
+++ tools/tools.go
@@ -11,6 +11,7 @@ package tools
 import (
 	_ "github.com/fyne-io/fyne-cross"
 	_ "github.com/goreleaser/nfpm/v2/cmd/nfpm"
+	_ "github.com/magefile/mage"
 	_ "golang.org/x/text/cmd/gotext"
 	_ "golang.org/x/tools/cmd/stringer"
 )
