4d6bbf3d40b84eaf837f063dd070708306177c2e
Joshua Rich <joshua.rich@gmail.com>
2024-05-25 15:53:45 +1000

    fix(ui): :bug: embed correct icon

--- internal/agent/ui/icon.go
+++ internal/agent/ui/icon.go
@@ -7,7 +7,7 @@ package ui
 
 import _ "embed"
 
-//go:embed assets/agent-dark.png
+//go:embed assets/go-hass-agent.png
 var hassIcon []byte
 
 // TrayIcon satisfies the fyne.Resource interface to represent the tray icon.
