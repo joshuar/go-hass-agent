24ce108263da73b6a1641eba7ad197caf8390255
Joshua Rich <joshua.rich@gmail.com>
2024-06-16 09:58:51 +1000

    build(devcontainer): :arrow_up: clean up additional installs and container configuration

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -4,10 +4,8 @@ set -e
 
 # Install go build packages
 go install golang.org/x/tools/cmd/stringer@latest
-go install github.com/fyne-io/fyne-cross@latest
 go install golang.org/x/text/cmd/gotext@latest
 go install github.com/matryer/moq@latest
-go install github.com/goreleaser/nfpm/v2/cmd/nfpm@latest
 go install github.com/tomwright/dasel/v2/cmd/dasel@latest
 go install github.com/sigstore/cosign/v2/cmd/cosign@latest
 
