a2060abe3f26379cc52e6400c63179ade0201f0b
Joshua Rich <joshua.rich@gmail.com>
2024-06-19 11:43:04 +1000

    build(devcontainer): :lock: use hashed versions for go command-line tools where possible

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -9,9 +9,9 @@ sudo ./build/scripts/install-deps arm arm64 amd64
 # Install go build packages
 go install golang.org/x/tools/cmd/stringer@latest
 go install golang.org/x/text/cmd/gotext@latest
-go install github.com/matryer/moq@latest
-go install github.com/tomwright/dasel/v2/cmd/dasel@latest
-go install github.com/sigstore/cosign/v2/cmd/cosign@latest
-go install github.com/magefile/mage@latest
+go install github.com/matryer/moq@0bf2e8a069abaefdfd07e4902d204441cca17298
+go install github.com/tomwright/dasel/v2/cmd/dasel@5d94a3049c2e81a410a6f48cf084c86c98393797
+go install github.com/sigstore/cosign/v2/cmd/cosign@fb651b4ddd8176bd81756fca2d988dd8611f514d
+go install github.com/magefile/mage@9e91a03eaa438d0d077aca5654c7757141536a60
 
 exit 0
