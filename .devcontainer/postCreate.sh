eda8037dc6139e45941b1c5c8e67089bf81cb891
Joshua Rich <joshua.rich@gmail.com>
2024-06-19 11:14:24 +1000

    build(devcontainer): :bug: ensure mage is installed in devcontainer

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -12,5 +12,6 @@ go install golang.org/x/text/cmd/gotext@latest
 go install github.com/matryer/moq@latest
 go install github.com/tomwright/dasel/v2/cmd/dasel@latest
 go install github.com/sigstore/cosign/v2/cmd/cosign@latest
+go install github.com/magefile/mage@latest
 
 exit 0
