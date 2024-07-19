256f865b1333e78dc517bc0d0b71e74a3c22bb25
Joshua Rich <joshua.rich@gmail.com>
2024-07-19 14:53:06 +1000

    build: :pushpin: fix versions of go tools

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -7,8 +7,8 @@ sudo ./build/scripts/enable-multiarch all
 sudo ./build/scripts/install-build-deps all 
 
 # Install go build packages
-go install golang.org/x/tools/cmd/stringer@latest
-go install golang.org/x/text/cmd/gotext@latest
+go install golang.org/x/tools/cmd/stringer@v0.23.0
+go install golang.org/x/text/cmd/gotext@v0.16.0
 go install github.com/matryer/moq@0bf2e8a069abaefdfd07e4902d204441cca17298
 go install github.com/tomwright/dasel/v2/cmd/dasel@5d94a3049c2e81a410a6f48cf084c86c98393797
 go install github.com/sigstore/cosign/v2/cmd/cosign@fb651b4ddd8176bd81756fca2d988dd8611f514d
