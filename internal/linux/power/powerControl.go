ff15d3437b3dcd547dfb51c2e6108e9e76dc9445
Joshua Rich <joshua.rich@gmail.com>
2024-08-12 15:41:21 +1000

    fix(linux): :bug: only provide power controls that are available on the device

--- internal/linux/power/powerControl.go
+++ internal/linux/power/powerControl.go
@@ -7,6 +7,7 @@ package power
 
 import (
 	"context"
+	"fmt"
 	"log/slog"
 
 	"github.com/eclipse/paho.golang/paho"
@@ -16,105 +17,128 @@ import (
 	"github.com/joshuar/go-hass-agent/pkg/linux/dbusx"
 )
 
-const (
-	dbusSessionLockMethod      = sessionInterface + ".Lock"
-	dbusSessionUnlockMethod    = sessionInterface + ".UnLock"
-	dbusSessionRebootMethod    = managerInterface + ".Reboot"
-	dbusSessionSuspendMethod   = managerInterface + ".Suspend"
-	dbusSessionHibernateMethod = managerInterface + ".Hibernate"
-	dbusSessionPowerOffMethod  = managerInterface + ".PowerOff"
-)
-
-type commandConfig struct {
-	name   string
-	icon   string
-	path   string
-	method string
+// powerController contains items that all power controls use.
+type powerController struct {
+	logger      *slog.Logger
+	bus         *dbusx.Bus
+	sessionPath string
 }
 
-//nolint:exhaustruct
-var commands = map[string]commandConfig{
-	"lock_session": {
-		name:   "Lock Session",
-		icon:   "mdi:eye-lock",
-		method: dbusSessionLockMethod,
-	},
-	"unlock_session": {
-		name:   "Unlock Session",
-		icon:   "mdi:eye-lock-open",
-		method: dbusSessionUnlockMethod,
-	},
-	"reboot": {
-		name:   "Reboot",
-		icon:   "mdi:restart",
-		path:   loginBasePath,
-		method: dbusSessionRebootMethod,
-	},
-	"suspend": {
-		name:   "Suspend",
-		icon:   "mdi:power-sleep",
-		path:   loginBasePath,
-		method: dbusSessionSuspendMethod,
-	},
-	"hibernate": {
-		name:   "Hibernate",
-		icon:   "mdi:power-sleep",
-		path:   loginBasePath,
-		method: dbusSessionHibernateMethod,
-	},
-	"power_off": {
-		name:   "Power Off",
-		icon:   "mdi:power",
-		path:   loginBasePath,
-		method: dbusSessionPowerOffMethod,
-	},
+// powerCommand represents a power command in the agent.
+type powerCommand struct {
+	callBack func(p *paho.Publish)
+	name     string
+	id       string
+	icon     string
 }
 
-func NewPowerControl(ctx context.Context, api *dbusx.DBusAPI, parentLogger *slog.Logger, device *mqtthass.Device) []*mqtthass.ButtonEntity {
-	logger := parentLogger.With(slog.String("controller", "power"))
+// generateCommands generates a list of power commands that are available on the
+// device running the agent.
+func (c *powerController) generateCommands(ctx context.Context) []powerCommand {
+	systemCommands := []powerCommand{
+		{
+			name: "Reboot",
+			id:   "Reboot",
+			icon: "mdi:restart",
+		},
+		{
+			name: "Suspend",
+			id:   "Suspend",
+			icon: "mdi:power-sleep",
+		},
+		{
+			name: "Hibernate",
+			id:   "Hibernate",
+			icon: "mdi:power-sleep",
+		},
+		{
+			name: "Power Off",
+			icon: "mdi:power",
+			id:   "PowerOff",
+		},
+	}
 
+	sessionCommands := []powerCommand{
+		{
+			name: "Lock Session",
+			id:   "Lock",
+			icon: "mdi:eye-lock",
+		},
+		{
+			name: "Unlock Session",
+			icon: "mdi:eye-lock-open",
+			id:   "UnLock",
+		},
+	}
+
+	availableCommands := make([]powerCommand, 0, len(systemCommands)+len(sessionCommands))
+
+	// Add available system power commands.
+	for _, config := range systemCommands {
+		// Check if this power method is available on the system.
+		available, _ := dbusx.GetData[string](ctx, c.bus, loginBasePath, loginBaseInterface, managerInterface+".Can"+config.id) //nolint:errcheck
+		if available == "yes" {
+			config.callBack = c.generatePowerControlCallback(ctx, config.name, loginBasePath, managerInterface+"."+config.id, true)
+			availableCommands = append(availableCommands, config)
+		}
+	}
+
+	// Add session power commands.
+	for _, config := range sessionCommands {
+		config.callBack = c.generatePowerControlCallback(ctx, config.name, c.sessionPath, sessionInterface+"."+config.id, nil)
+		availableCommands = append(availableCommands, config)
+	}
+
+	return availableCommands
+}
+
+// generatePowerControlCallback creates an MQTT callback function that can
+// execute the appropriate D-Bus call to issue a power command on the device.
+func (c *powerController) generatePowerControlCallback(ctx context.Context, name, path, method string, arg any) func(p *paho.Publish) {
+	if arg != nil {
+		return func(_ *paho.Publish) {
+			if err := c.bus.Call(ctx, path, loginBaseInterface, method, arg); err != nil {
+				c.logger.Warn("Could not issue power control.", slog.String("control", name), slog.Any("error", err))
+			}
+		}
+	}
+
+	return func(_ *paho.Publish) {
+		if err := c.bus.Call(ctx, path, loginBaseInterface, method); err != nil {
+			c.logger.Warn("Could not issue power control.", slog.String("control", name), slog.Any("error", err))
+		}
+	}
+}
+
+//nolint:lll
+func NewPowerControl(ctx context.Context, api *dbusx.DBusAPI, parentLogger *slog.Logger, device *mqtthass.Device) ([]*mqtthass.ButtonEntity, error) {
 	bus, err := api.GetBus(ctx, dbusx.SystemBus)
 	if err != nil {
-		logger.Warn("Cannot create power controls.", "error", err.Error())
-
-		return nil
+		return nil, fmt.Errorf("cannot create power controls: %w", err)
 	}
 
 	sessionPath, err := bus.GetSessionPath(ctx)
 	if err != nil {
-		logger.Warn("Cannot create power controls.", "error", err.Error())
-
-		return nil
+		return nil, fmt.Errorf("cannot create power controls: %w", err)
 	}
 
-	entities := make([]*mqtthass.ButtonEntity, 0, len(commands))
+	controller := &powerController{
+		logger:      parentLogger.WithGroup("power_control"),
+		bus:         bus,
+		sessionPath: sessionPath,
+	}
 
-	for cmdName, cmdInfo := range commands {
-		var callback func(p *paho.Publish)
-		if cmdInfo.path == "" {
-			callback = func(_ *paho.Publish) {
-				err := bus.Call(ctx, sessionPath, loginBaseInterface, cmdInfo.method)
-				if err != nil {
-					logger.Warn("Could not perform power control action.", "action", cmdInfo.name, "error", err.Error())
-				}
-			}
-		} else {
-			callback = func(_ *paho.Publish) {
-				err := bus.Call(ctx, cmdInfo.path, loginBaseInterface, cmdInfo.method, true)
-				if err != nil {
-					logger.Warn("Could not power off session.", "error", err.Error())
-				}
-			}
-		}
+	var entities []*mqtthass.ButtonEntity //nolint:prealloc
 
+	for _, command := range controller.generateCommands(ctx) {
 		entities = append(entities,
 			mqtthass.AsButton(
-				mqtthass.NewEntity(preferences.AppName, cmdInfo.name, device.Name+"_"+cmdName).
+				mqtthass.NewEntity(preferences.AppName, command.name, command.name).
 					WithOriginInfo(preferences.MQTTOrigin()).
 					WithDeviceInfo(device).
-					WithIcon(cmdInfo.icon).
-					WithCommandCallback(callback)))
+					WithIcon(command.icon).
+					WithCommandCallback(command.callBack)))
 	}
 
-	return entities
+	return entities, nil
 }
