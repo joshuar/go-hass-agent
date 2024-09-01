c87995934abe38173527a14a1263e65a4788d3bf
Joshua Rich <joshua.rich@gmail.com>
2024-09-01 18:02:45 +1000

    perf(linux): :bug: network connection handling improvements
    - ensure any goroutines spawned are shut down correctly
    - add some additional debug logging

--- internal/linux/net/networkConnection.go
+++ internal/linux/net/networkConnection.go
@@ -36,9 +36,10 @@ const (
 )
 
 type connectionsWorker struct {
-	bus  *dbusx.Bus
-	list []dbus.ObjectPath
-	mu   sync.Mutex
+	bus    *dbusx.Bus
+	logger *slog.Logger
+	list   []dbus.ObjectPath
+	mu     sync.Mutex
 }
 
 func (w *connectionsWorker) track(path dbus.ObjectPath) {
@@ -66,85 +67,92 @@ func (w *connectionsWorker) Sensors(_ context.Context) ([]sensor.Details, error)
 	return nil, linux.ErrUnimplemented
 }
 
-//nolint:cyclop,mnd
+//nolint:mnd
 //revive:disable:function-length
 func (w *connectionsWorker) Events(ctx context.Context) (chan sensor.Details, error) {
 	sensorCh := make(chan sensor.Details)
-	logger := slog.With(slog.String("worker", netConnWorkerID))
-
-	connectionlist, err := dbusx.NewProperty[[]dbus.ObjectPath](w.bus, dBusNMPath, dBusNMObj, dBusNMObj+".ActiveConnections").Get()
-	if err != nil {
-		logger.Debug("Error getting active connections from D-Bus", slog.Any("error", err))
-	}
-
-	w.list = connectionlist
-
-	handleConn := func(path dbus.ObjectPath) {
-		var conn *connection
-
-		conn, err = newConnection(w.bus, path)
-		if err != nil {
-			logger.Debug("Unable to monitor connection.", slog.Any("error", err))
-
-			return
-		}
-		// Ignore loopback.
-		if conn.name == "lo" {
-			return
-		}
-		// Start monitoring the connection. Pass any sensor updates from the
-		// connection through the sensor channel.
-		go func() {
-			for s := range conn.monitor(ctx, w.bus) {
-				sensorCh <- s
-			}
-
-			w.untrack(path)
-		}()
-	}
+	connCtx, connCancel := context.WithCancel(ctx)
 
 	triggerCh, err := dbusx.NewWatch(
 		dbusx.MatchPathNamespace(dbusNMActiveConnPath),
 		dbusx.MatchInterface(dbusNMActiveConnIntr),
 		dbusx.MatchMembers("StateChanged"),
-	).Start(ctx, w.bus)
+	).Start(connCtx, w.bus)
 	if err != nil {
 		close(sensorCh)
+		connCancel()
 
 		return sensorCh, fmt.Errorf("failed to create network connections D-Bus watch: %w", err)
 	}
 
 	go func() {
 		defer close(sensorCh)
+		w.logger.Debug("Watching for network connections.")
 
-		for {
-			select {
-			case <-ctx.Done():
-				return
-			case event := <-triggerCh:
-				connectionPath := dbus.ObjectPath(event.Path)
-				// If this connection is in the process of deactivating, don't
-				// start tracking it.
-				if state, stateChange := event.Content[0].(uint32); stateChange {
-					if state > 2 {
-						continue
-					}
-				}
-				// Track all activating/new connections.
-				if !w.isTracked(connectionPath) {
-					w.track(connectionPath)
-					handleConn(connectionPath)
+		for event := range triggerCh {
+			connectionPath := dbus.ObjectPath(event.Path)
+			// If this connection is in the process of deactivating, don't
+			// start tracking it.
+			if state, stateChange := event.Content[0].(uint32); stateChange {
+				if state > 2 {
+					continue
 				}
 			}
+			// Track all activating/new connections.
+			if err = w.handleConnection(connCtx, connectionPath, sensorCh); err != nil {
+				w.logger.Debug("Could not monitor connection.", slog.Any("error", err))
+			}
 		}
+
+		w.logger.Debug("Stopped watching network connections.")
+	}()
+
+	go func() {
+		defer connCancel()
+		<-ctx.Done()
+		w.logger.Debug("Stopped events.")
 	}()
 
 	// monitor all current active connections
-	for _, path := range w.list {
-		handleConn(path)
+	connectionlist, err := dbusx.NewProperty[[]dbus.ObjectPath](w.bus, dBusNMPath, dBusNMObj, dBusNMObj+".ActiveConnections").Get()
+	if err != nil {
+		w.logger.Debug("Error getting active connections from D-Bus", slog.Any("error", err))
+	} else {
+		for _, path := range connectionlist {
+			if err := w.handleConnection(connCtx, path, sensorCh); err != nil {
+				w.logger.Debug("Could not monitor connection.", slog.Any("error", err))
+			}
+		}
 	}
 
 	return sensorCh, nil
+}
+
+func (w *connectionsWorker) handleConnection(ctx context.Context, path dbus.ObjectPath, sensorCh chan sensor.Details) error {
+	conn, err := newConnection(w.bus, path)
+	if err != nil {
+		return fmt.Errorf("could not create connection: %w", err)
+	}
+	// Ignore loopback or already tracked connections.
+	if conn.name == "lo" || w.isTracked(path) {
+		slog.Debug("Ignoring connection.", slog.String("connection", conn.name))
+
+		return nil
+	}
+
+	// Start monitoring the connection. Pass any sensor updates from the
+	// connection through the sensor channel.
+	go func() {
+		w.track(path)
+
+		for s := range conn.monitor(ctx, w.bus) {
+			sensorCh <- s
+		}
+
+		w.untrack(path)
+	}()
+
+	return nil
 }
 
 func NewConnectionWorker(ctx context.Context, api *dbusx.DBusAPI) (*linux.SensorWorker, error) {
@@ -155,7 +163,8 @@ func NewConnectionWorker(ctx context.Context, api *dbusx.DBusAPI) (*linux.Sensor
 
 	return &linux.SensorWorker{
 			Value: &connectionsWorker{
-				bus: bus,
+				bus:    bus,
+				logger: slog.With(slog.String("worker", netConnWorkerID)),
 			},
 			WorkerID: netConnWorkerID,
 		},
