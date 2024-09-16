dfc2861657178c8f272d5112cf6fa298cbd9bfb5
Joshua Rich <joshua.rich@gmail.com>
2024-09-16 21:38:52 +1000

    refactor(agent): :truck: move version sensor code

--- internal/agent/version_sensor.go
+++ internal/agent/version_sensor.go
@@ -10,11 +10,35 @@ import (
 	"context"
 
 	"github.com/joshuar/go-hass-agent/internal/hass/sensor"
+	"github.com/joshuar/go-hass-agent/internal/hass/sensor/types"
+	"github.com/joshuar/go-hass-agent/internal/preferences"
 )
 
 const (
 	versionWorkerID = "agent_version_sensor"
 )
+
+type version string
+
+func (v *version) Name() string { return "Go Hass Agent Version" }
+
+func (v *version) ID() string { return "agent_version" }
+
+func (v *version) Icon() string { return "mdi:face-agent" }
+
+func (v *version) SensorType() types.SensorClass { return types.Sensor }
+
+func (v *version) DeviceClass() types.DeviceClass { return 0 }
+
+func (v *version) StateClass() types.StateClass { return 0 }
+
+func (v *version) State() any { return preferences.AppVersion }
+
+func (v *version) Units() string { return "" }
+
+func (v *version) Category() string { return types.CategoryDiagnostic }
+
+func (v *version) Attributes() map[string]any { return nil }
 
 type versionWorker struct {
 	version
