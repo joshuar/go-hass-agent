095f06947ed787ca096e65d49e242f3369387e71
Joshua Rich <joshua.rich@gmail.com>
2024-09-27 21:54:22 +1000

    feat(linux): :sparkles: switch total cpu context switches and processes created sensors from totals to rates
    - total cpu context switches changes to cpu context switch rate
    - total processes created changes to process creation rate
    - split cpu usage worker and sensor code into separate files

--- internal/linux/cpu/usage.go
+++ internal/linux/cpu/usage.go
@@ -3,17 +3,9 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-//revive:disable:unused-receiver
 package cpu
 
 import (
-	"bufio"
-	"bytes"
-	"context"
-	"errors"
-	"fmt"
-	"os"
-	"path/filepath"
 	"strconv"
 	"strings"
 	"time"
@@ -25,56 +17,47 @@ import (
 	"github.com/joshuar/go-hass-agent/internal/linux"
 )
 
-const (
-	usageUpdateInterval = 10 * time.Second
-	usageUpdateJitter   = 500 * time.Millisecond
-
-	usageWorkerID = "cpu_usage_sensors"
-
-	totalCPUString = "cpu"
-)
-
-var ErrParseCPUUsage = errors.New("could not parse CPU usage")
-
 //nolint:lll
 var times = [...]string{"user_time", "nice_time", "system_time", "idle_time", "iowait_time", "irq_time", "softirq_time", "steal_time", "guest_time", "guest_nice_time"}
 
-type usageWorker struct {
-	boottime time.Time
-	path     string
-	linux.PollingSensorWorker
-	clktck int64
+type rateSensor struct {
+	*sensor.Entity
+	prevState uint64
 }
 
-func (w *usageWorker) UpdateDelta(_ time.Duration) {}
+func (s *rateSensor) update(delta time.Duration, valueStr string) {
+	valueInt, _ := strconv.ParseUint(valueStr, 10, 64) //nolint:errcheck // if we can't parse it, value will be 0.
 
-func (w *usageWorker) Sensors(_ context.Context) ([]sensor.Entity, error) {
-	return w.getStats()
+	if uint64(delta.Seconds()) > 0 {
+		s.State = (valueInt - s.prevState) / uint64(delta.Seconds()) / 2
+	} else {
+		s.State = 0
+	}
+
+	s.Attributes["Total"] = valueInt
+
+	s.prevState = valueInt
 }
 
-func NewUsageWorker(ctx context.Context) (*linux.PollingSensorWorker, error) {
-	worker := linux.NewPollingWorker(usageWorkerID, usageUpdateInterval, usageUpdateJitter)
-
-	clktck, found := linux.CtxGetClkTck(ctx)
-	if !found {
-		return worker, fmt.Errorf("%w: no clktck value", linux.ErrInvalidCtx)
+func newRateSensor(name, icon, units string) *rateSensor {
+	return &rateSensor{
+		Entity: &sensor.Entity{
+			Name:       name,
+			StateClass: types.StateClassMeasurement,
+			Category:   types.CategoryDiagnostic,
+			Units:      units,
+			EntityState: &sensor.EntityState{
+				ID:   strcase.ToSnake(name),
+				Icon: icon,
+				Attributes: map[string]any{
+					"data_source": linux.DataSrcProcfs,
+				},
+			},
+		},
 	}
-
-	boottime, found := linux.CtxGetBoottime(ctx)
-	if !found {
-		return worker, fmt.Errorf("%w: no boottime value", linux.ErrInvalidCtx)
-	}
-
-	worker.PollingType = &usageWorker{
-		path:     filepath.Join(linux.ProcFSRoot, "stat"),
-		boottime: boottime,
-		clktck:   clktck,
-	}
-
-	return worker, nil
 }
 
-func (w *usageWorker) newUsageSensor(details []string, category types.Category) sensor.Entity {
+func newUsageSensor(clktck int64, details []string, category types.Category) sensor.Entity {
 	var name, id string
 
 	switch {
@@ -87,7 +70,7 @@ func (w *usageWorker) newUsageSensor(details []string, category types.Category) 
 		id = "core_" + num + "_cpu_usage"
 	}
 
-	value, attributes := generateUsageValues(w.clktck, details[1:])
+	value, attributes := generateUsageValues(clktck, details[1:])
 
 	return sensor.Entity{
 		Name:       name,
@@ -128,7 +111,7 @@ func generateUsageValues(clktck int64, details []string) (float64, map[string]an
 	return value, attrs
 }
 
-func (w *usageWorker) newCountSensor(name, icon, valueStr string) sensor.Entity {
+func newCountSensor(name, icon, valueStr string) sensor.Entity {
 	valueInt, _ := strconv.Atoi(valueStr) //nolint:errcheck // if we can't parse it, value will be 0.
 
 	return sensor.Entity{
@@ -144,49 +127,4 @@ func (w *usageWorker) newCountSensor(name, icon, valueStr string) sensor.Entity 
 			},
 		},
 	}
-}
-
-func (w *usageWorker) getStats() ([]sensor.Entity, error) {
-	var sensors []sensor.Entity
-
-	statsFH, err := os.Open(w.path)
-	if err != nil {
-		return nil, fmt.Errorf("fetch cpu usage: %w", err)
-	}
-
-	defer statsFH.Close()
-
-	statsFile := bufio.NewScanner(statsFH)
-	for statsFile.Scan() {
-		// Set up word scanner for line.
-		line := bufio.NewScanner(bytes.NewReader(statsFile.Bytes()))
-		line.Split(bufio.ScanWords)
-		// Split line by words
-		var cols []string
-		for line.Scan() {
-			cols = append(cols, line.Text())
-		}
-
-		if len(cols) == 0 {
-			return sensors, ErrParseCPUUsage
-		}
-		// Create a sensor depending on the line.
-		switch {
-		case cols[0] == totalCPUString:
-			sensors = append(sensors, w.newUsageSensor(cols, types.CategoryDefault))
-		case strings.Contains(cols[0], "cpu"):
-			sensors = append(sensors, w.newUsageSensor(cols, types.CategoryDiagnostic))
-			sensors = append(sensors, newCPUFreqSensor(cols[0]))
-		case cols[0] == "ctxt":
-			sensors = append(sensors, w.newCountSensor("Total CPU Context Switches", "mdi:counter", cols[1]))
-		case cols[0] == "processes":
-			sensors = append(sensors, w.newCountSensor("Total Processes Created", "mdi:application-cog", cols[1]))
-		case cols[0] == "procs_running":
-			sensors = append(sensors, w.newCountSensor("Processes Running", "mdi:application-cog", cols[1]))
-		case cols[0] == "procs_blocked":
-			sensors = append(sensors, w.newCountSensor("Processes Blocked", "mdi:application-cog", cols[1]))
-		}
-	}
-
-	return sensors, nil
 }
