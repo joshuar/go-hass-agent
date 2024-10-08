b349670b06aeec46c9cdff13a0ab86ebd8289568
Joshua Rich <joshua.rich@gmail.com>
2024-10-08 20:53:13 +1000

    build(go): :heavy_minus_sign: remove command-line tools that can just be installed in the dev container from tools deps

--- tools.go
+++ tools.go
@@ -12,13 +12,7 @@ package main
 
 import (
 	_ "github.com/davecgh/go-spew/spew"
-	_ "github.com/google/go-licenses"
-	_ "github.com/goreleaser/nfpm/v2/cmd/nfpm"
-	_ "github.com/magefile/mage"
 	_ "github.com/matryer/moq"
-	_ "github.com/sigstore/cosign/v2/cmd/cosign"
 	_ "github.com/yassinebenaid/godump"
-	_ "go.uber.org/nilaway/cmd/nilaway"
 	_ "golang.org/x/tools/cmd/stringer"
-	_ "honnef.co/go/tools/cmd/staticcheck"
 )
