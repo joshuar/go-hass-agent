d40b6afb551e3d13d56b737583e2ded00ece4dd9
Joshua Rich <joshua.rich@gmail.com>
2024-10-09 14:27:25 +1000

    build(go): :heavy_plus_sign: add mage as tools dependency

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -5,10 +5,6 @@ set -e
 sudo ./build/scripts/enable-multiarch all
 sudo ./build/scripts/install-build-deps all ubuntu
 
-# Install go build packages
-go install github.com/magefile/mage@9e91a03eaa438d0d077aca5654c7757141536a60                 # v1.15.0
-go install github.com/sigstore/cosign/v2/cmd/cosign@b5e7dc123a272080f4af4554054797296271e902 # v2.4.0
-
 # Install and configure starship
 curl -sS https://starship.rs/install.sh | sh -s -- -y || exit -1
 mkdir -p ~/.config/fish
