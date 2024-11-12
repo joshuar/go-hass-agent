1829217454e7acad35a0c91106de69705ee9b7e7
Joshua Rich <joshua.rich@gmail.com>
2024-11-12 15:07:16 +1000

    build(devcontainer): :bug: install runtime deps in devcontainer

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -4,6 +4,7 @@ set -e
 
 sudo ./build/scripts/enable-multiarch all
 sudo ./build/scripts/install-build-deps all ubuntu
+sudo ./build/scripts/install-run-deps linux/amd64
 
 # Install and configure starship
 curl -sS https://starship.rs/install.sh | sh -s -- -y || exit -1
