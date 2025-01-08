ee006372fdfdbccbefab66e0621b8a0f294913ab
Joshua Rich <joshua.rich@gmail.com>
2025-01-08 22:31:12 +1000

    refactor(cli): :bulb: add additional comments

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -20,13 +20,16 @@ const (
 //go:embed assets
 var content embed.FS
 
+// CmdOpts are the global command-line options common across all commands.
 type CmdOpts struct {
 	Logger   *slog.Logger
 	Headless bool
 }
 
+// Option represents a command-line option.
 type Option func(*CmdOpts) *CmdOpts
 
+// AddOptions adds the given options to a command.
 func AddOptions(options ...Option) *CmdOpts {
 	commandOptions := &CmdOpts{}
 	for _, option := range options {
@@ -36,6 +39,7 @@ func AddOptions(options ...Option) *CmdOpts {
 	return commandOptions
 }
 
+// RunHeadless sets the headless command-line option.
 func RunHeadless(opt bool) Option {
 	return func(ctx *CmdOpts) *CmdOpts {
 		ctx.Headless = opt
@@ -43,6 +47,8 @@ func RunHeadless(opt bool) Option {
 	}
 }
 
+// WithAppID sets the given ID as the application ID. Useful during development
+// and debugging to change the path to the agent preferences.
 func WithAppID(id string) Option {
 	return func(ctx *CmdOpts) *CmdOpts {
 		preferences.SetAppID(id)
@@ -50,6 +56,7 @@ func WithAppID(id string) Option {
 	}
 }
 
+// WithLogger sets the logger that will be inherited by the command.
 func WithLogger(logger *slog.Logger) Option {
 	return func(ctx *CmdOpts) *CmdOpts {
 		ctx.Logger = logger
@@ -57,6 +64,7 @@ func WithLogger(logger *slog.Logger) Option {
 	}
 }
 
+// HeadlessFlag represents whether the agent is running headless or not.
 type HeadlessFlag bool
 
 func (f *HeadlessFlag) AfterApply() error {
