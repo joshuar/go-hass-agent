0fd43b231f4d67efeeafcc773f1a8955b9ca6da6
Joshua Rich <joshua.rich@gmail.com>
2025-02-22 12:01:35 +1000

    test(hass): :white_check_mark: fix registration tests

--- internal/hass/registration_test.go
+++ internal/hass/registration_test.go
@@ -7,14 +7,17 @@ import (
 	"testing"
 
 	"github.com/joshuar/go-hass-agent/internal/components/preferences"
+	"github.com/joshuar/go-hass-agent/internal/hass/api"
 )
 
 func Test_generateAPIURL(t *testing.T) {
 	webhookID := "testID"
 	token := "testToken"
+	cloudhookURL := "http://localhost:8123/cloudhook"
+	remoteUIURL := "http://localhost:8123/remoteui"
 
 	type args struct {
-		response *deviceRegistrationResponse
+		response *api.DeviceRegistrationResponse
 		request  *preferences.Registration
 	}
 	tests := []struct {
@@ -26,7 +29,7 @@ func Test_generateAPIURL(t *testing.T) {
 		{
 			name: "use cloudhook",
 			args: args{
-				response: &deviceRegistrationResponse{CloudhookURL: "http://localhost:8123/cloudhook", WebhookID: webhookID},
+				response: &api.DeviceRegistrationResponse{CloudhookURL: &cloudhookURL, WebhookID: webhookID},
 				request:  &preferences.Registration{Server: "http://localhost", Token: token},
 			},
 			want: "http://localhost:8123/cloudhook",
@@ -34,7 +37,7 @@ func Test_generateAPIURL(t *testing.T) {
 		{
 			name: "use remoteui",
 			args: args{
-				response: &deviceRegistrationResponse{RemoteUIURL: "http://localhost:8123/remoteui", WebhookID: webhookID},
+				response: &api.DeviceRegistrationResponse{RemoteUIURL: &remoteUIURL, WebhookID: webhookID},
 				request:  &preferences.Registration{Server: "http://localhost", Token: token},
 			},
 			want: "http://localhost:8123/remoteui/api/webhook/" + webhookID,
@@ -42,7 +45,7 @@ func Test_generateAPIURL(t *testing.T) {
 		{
 			name: "ignoreURLs",
 			args: args{
-				response: &deviceRegistrationResponse{RemoteUIURL: "http://localhost:8123/remoteui", WebhookID: webhookID},
+				response: &api.DeviceRegistrationResponse{RemoteUIURL: &remoteUIURL, WebhookID: webhookID},
 				request:  &preferences.Registration{Server: "http://localhost", Token: token, IgnoreHassURLs: true},
 			},
 			want: "http://localhost/api/webhook/" + webhookID,
@@ -50,7 +53,7 @@ func Test_generateAPIURL(t *testing.T) {
 		{
 			name: "no cloudhook or remoteui",
 			args: args{
-				response: &deviceRegistrationResponse{WebhookID: webhookID},
+				response: &api.DeviceRegistrationResponse{WebhookID: webhookID},
 				request:  &preferences.Registration{Server: "http://localhost", Token: token},
 			},
 			want: "http://localhost/api/webhook/" + webhookID,
