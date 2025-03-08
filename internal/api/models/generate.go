e418753d7bf4182bb05909d15ba2e98bcefede01
Joshua Rich <joshua.rich@gmail.com>
2025-03-08 10:16:33 +1000

    build(schemas): :truck: move schemas to dedicated folder/package

--- internal/api/models/generate.go
+++
@@ -1,5 +0,0 @@
-// Copyright 2025 Joshua Rich <joshua.rich@gmail.com>.
-// SPDX-License-Identifier: MIT
-
-//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen -config models-cfg.yaml models.yaml
-package api
