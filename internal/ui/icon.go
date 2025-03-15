62e30b03f6fbbc46d6a910cf978772a8fdcd1836
Joshua Rich <joshua.rich@gmail.com>
2025-03-15 11:48:23 +1000

    refactor: :fire: remove deprecated agent monolith code

--- internal/ui/icon.go
+++ internal/ui/icon.go
@@ -11,13 +11,13 @@ import _ "embed"
 //go:embed assets/go-hass-agent.png
 var hassIcon []byte
 
-// TrayIcon satisfies the fyne.Resource interface to represent the tray icon.
-type TrayIcon struct{}
+// trayIcon satisfies the fyne.Resource interface to represent the tray icon.
+type trayIcon struct{}
 
-func (i *TrayIcon) Name() string {
+func (i *trayIcon) Name() string {
 	return "TrayIcon"
 }
 
-func (i *TrayIcon) Content() []byte {
+func (i *trayIcon) Content() []byte {
 	return hassIcon
 }
