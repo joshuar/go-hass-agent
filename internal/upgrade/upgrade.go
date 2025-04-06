3aa8a8fcdb91e9de0bfd25c374e607729ece8344
Joshua Rich <joshua.rich@gmail.com>
2025-04-06 21:50:40 +1000

    refactor(upgrade): :rotating_light: more linter clean-up

--- internal/upgrade/upgrade.go
+++ internal/upgrade/upgrade.go
@@ -1,6 +1,7 @@
 // Copyright 2025 Joshua Rich <joshua.rich@gmail.com>.
 // SPDX-License-Identifier: MIT
 
+// Package upgrade provides some limited assistance for automatic major version upgrades.
 package upgrade
 
 import (
@@ -10,6 +11,7 @@ import (
 	"github.com/joshuar/go-hass-agent/internal/components/logging"
 )
 
+// Run will run the upgrade command.
 func Run(ctx context.Context) error {
 	logging.FromContext(ctx).Info("Checking for and attempting pre v10.0.0 upgrades...")
 	// Perform pre v10.0.0 upgrades...
