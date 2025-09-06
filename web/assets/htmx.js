3e167fc2076baa5773929efd0b5376d1b276b759
Joshua Rich <joshua.rich@gmail.com>
2025-09-06 04:20:50 +0000

    feat: :bento: add additional frontend assets

---
+++ web/assets/htmx.js
@@ -0,0 +1,5 @@
+// Copyright 2025 Joshua Rich <joshua.rich@gmail.com>.
+// SPDX-License-Identifier: MIT
+
+import htmx from "htmx.org";
+window.htmx = htmx
---
+++ web/assets/scripts.js
@@ -0,0 +1,9 @@
+// Copyright 2025 Joshua Rich <joshua.rich@gmail.com>.
+// SPDX-License-Identifier: MIT
+
+// htmx
+import 'htmx.org'
+import './htmx.js'
+// hyperscript
+import _hyperscript from 'hyperscript.org/dist/_hyperscript.js'
+_hyperscript.browserInit()
