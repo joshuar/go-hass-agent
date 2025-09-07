9275f3d1dbf81f4e9d489f90d95ffa2adcec893e
Joshua Rich <joshua.rich@gmail.com>
2025-09-07 01:31:14 +0000

    feat(device): :sparkles: re-add device info methods

---
+++ device/info.go
@@ -0,0 +1,74 @@
+// Copyright (c) 2024 Joshua Rich <joshua.rich@gmail.com>
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+package device
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/gofrs/uuid/v5"
+	"github.com/jaypipes/ghw"
+)
+
+const (
+	unknownVendor        = "Unknown Vendor"
+	unknownModel         = "Unknown Model"
+	unknownDistro        = "Unknown Distro"
+	unknownDistroVersion = "Unknown Version"
+	unknownValue         = "unknown"
+	defaultHostname      = "localhost"
+)
+
+// ErrUnsupportedHardware is returned when the hardware does not support the method or action.
+var ErrUnsupportedHardware = errors.New("unsupported hardware")
+
+// Chassis will return the chassis type of the machine, such as "desktop" or
+// "laptop". If this cannot be retrieved, it will return "unknown".
+func Chassis() (string, error) {
+	chassisInfo, err := ghw.Chassis(ghw.WithDisableWarnings())
+	if err != nil || chassisInfo == nil {
+		return unknownValue, fmt.Errorf("could not determine chassis type: %w", err)
+	}
+
+	return chassisInfo.TypeDescription, nil
+}
+
+// GetHostname retrieves the hostname of the device running the agent, or
+// localhost if that doesn't work.
+func GetHostname() (string, error) {
+	hostname, err := os.Hostname()
+	if err != nil {
+		return defaultHostname, fmt.Errorf("could not retrieve hostname: %w", err)
+	}
+
+	shortHostname, _, _ := strings.Cut(hostname, ".")
+
+	return shortHostname, nil
+}
+
+// GetHWProductInfo retrieves the model and vendor of the machine. If these
+// cannot be retrieved or cannot be found, they will be set to default unknown
+// strings.
+func GetHWProductInfo() (string, string, error) {
+	product, err := ghw.Product(ghw.WithDisableWarnings())
+	if err != nil || product == nil {
+		return unknownModel, unknownVendor, fmt.Errorf("could not retrieve hardware information: %w", err)
+	}
+
+	return product.Name, product.Vendor, nil
+}
+
+// NewDeviceID create a new device ID. It will be a randomly generated UUIDv4.
+func NewDeviceID() (string, error) {
+	deviceID, err := uuid.NewV4()
+	if err != nil {
+		return "", fmt.Errorf("could not retrieve a machine ID: %w", err)
+	}
+
+	return deviceID.String(), nil
+}
---
+++ device/info_linux.go
@@ -0,0 +1,99 @@
+// Copyright (c) 2024 Joshua Rich <joshua.rich@gmail.com>
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+package device
+
+import (
+	"fmt"
+	"syscall"
+
+	"github.com/joshuar/go-hass-agent/pkg/linux/whichdistro"
+)
+
+// GetOSID will retrieve the distribution ID and version ID. These are
+// suitable for usage as part of identifiers and variables. See also
+// GetDistroDetails.
+func GetOSID() (string, string, error) {
+	var distroName, distroVersion string
+
+	osReleaseInfo, err := whichdistro.GetOSRelease()
+	if err != nil {
+		return unknownDistro, unknownDistroVersion,
+			fmt.Errorf("could not read /etc/os-release: %w", err)
+	}
+
+	if v, ok := osReleaseInfo.GetValue("ID"); !ok {
+		distroName = unknownDistro
+	} else {
+		distroName = v
+	}
+
+	if v, ok := osReleaseInfo.GetValue("VERSION_ID"); !ok {
+		distroVersion = unknownDistroVersion
+	} else {
+		distroVersion = v
+	}
+
+	return distroName, distroVersion, nil
+}
+
+// GetOSDetails will retrieve the distribution name and version. The values
+// are pretty-printed and may not be suitable for usage as identifiers and
+// variables. See also GetDistroID.
+func GetOSDetails() (string, string, error) {
+	var (
+		distroName, distroVersion string
+		value                     string
+		found                     bool
+	)
+
+	osReleaseInfo, err := whichdistro.GetOSRelease()
+	if err != nil {
+		return unknownDistro, unknownDistroVersion,
+			fmt.Errorf("could not read /etc/os-release: %w", err)
+	}
+
+	if value, found = osReleaseInfo.GetValue("NAME"); found {
+		distroName = value
+	} else if value, found = osReleaseInfo.GetValue("ID"); found {
+		distroName = value
+	} else {
+		distroName = unknownDistro
+	}
+
+	if value, found = osReleaseInfo.GetValue("VERSION"); found {
+		distroVersion = value
+	} else if value, found = osReleaseInfo.GetValue("VERSION_ID"); found {
+		distroVersion = value
+	} else {
+		distroVersion = unknownDistroVersion
+	}
+
+	return distroName, distroVersion, nil
+}
+
+// GetKernelVersion will retrieve the kernel version.
+//
+//nolint:prealloc
+func GetKernelVersion() (string, error) {
+	var utsname syscall.Utsname
+
+	var versionBytes []byte
+
+	err := syscall.Uname(&utsname)
+	if err != nil {
+		return unknownValue, fmt.Errorf("could not retrieve kernel version: %w", err)
+	}
+
+	for _, v := range utsname.Release {
+		if v == 0 {
+			continue
+		}
+
+		versionBytes = append(versionBytes, uint8(v)) // #nosec: G115
+	}
+
+	return string(versionBytes), nil
+}
