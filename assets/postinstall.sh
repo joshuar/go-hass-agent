dec52e6fe88b850fda0060b64c6aa33c1515b26d
Joshua Rich <joshua.rich@gmail.com>
2025-09-28 11:36:32 +0000

    build: :fire: clean up postinstall for package installs

--- assets/postinstall.sh
+++ assets/postinstall.sh
@@ -3,37 +3,11 @@
 
 #!/bin/sh
 
-cleanInstall() {
-    # Set capabilities required for some workers if `setcap` is available. If not, some workers will not be able to run.
+# Set capabilities required for some workers if `setcap` is available. If not, some workers will not be able to run.
+setCapabilities() {
     if type setcap >/dev/null; then
         setcap 'cap_sys_rawio,cap_sys_admin,cap_mknod,cap_dac_override=+ep' /usr/bin/go-hass-agent
     fi
 }
 
-# upgrade() {
-# }
-
-# Step 2, check if this is a clean install or an upgrade
-action="$1"
-if  [ "$1" = "configure" ] && [ -z "$2" ]; then
-  # Alpine linux does not pass args, and deb passes $1=configure
-  action="install"
-elif [ "$1" = "configure" ] && [ -n "$2" ]; then
-    # deb passes $1=configure $2=<current version>
-    action="upgrade"
-fi
-
-case "$action" in
-  "1" | "install")
-    cleanInstall
-    ;;
-#   "2" | "upgrade")
-#     upgrade
-#     ;;
-  *)
-    # $1 == version being installed
-    printf "\033[32m Alpine\033[0m"
-    cleanInstall
-    ;;
-esac
-
+setCapabilities
