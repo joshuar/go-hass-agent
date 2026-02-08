fa5933ef63cb1a7cf9352709300e66e534658169
Joshua Rich <joshua.rich@gmail.com>
2026-02-08 00:35:50 +0000

    build(devcontainer): :recycle: devcontainer improvements
    - fix go setup in postcreate script
    - run dbus session daemon in devcontainer

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -13,20 +13,19 @@ echo 'eval "$(starship init bash)"' >>~/.bashrc
 
 cd /workspace
 
-
 # Update JS packages.
 npm update || exit -1
 
 # Install Go packages.
-go mod tidy
-go install github.com/air-verse/air@latest
-go install github.com/a-h/templ/cmd/templ@latest
-go install golang.org/x/tools/gopls@latest
-go install github.com/sigstore/cosign/v3/cmd/cosign@latest
-curl -sSfL https://golangci-lint.run/install.sh | sh -s -- -b $(go env GOPATH)/bin v2.8.0
-cd /tmp && \
-    ~/go/bin/golangci-lint custom && \
-    mv golangci-lint $(go env GOPATH)/bin/golangci-lint-v2
+export PATH="$HOME/go/bin:/go/bin:/usr/local/go/bin:$PATH" && \
+    go mod tidy && \
+    go install github.com/air-verse/air@latest && \
+    go install github.com/a-h/templ/cmd/templ@latest && \
+    go install golang.org/x/tools/gopls@latest && \
+    go install github.com/sigstore/cosign/v3/cmd/cosign@latest && \
+    curl -sSfL https://golangci-lint.run/install.sh | sh -s -- -b $(go env GOPATH)/bin v2.8.0 && \
+    golangci-lint custom && \
+    mv /tmp/golangci-lint-v2 $(go env GOPATH)/bin/golangci-lint-v2
 
 # Set up fish shell.
 mkdir -p ~/.config/fish
