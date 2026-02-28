dbb7e207585021716a312537288283e1bb1cc71f
Joshua Rich <joshua.rich@gmail.com>
2026-02-28 00:37:02 +0000

    refactor(dbusx): :recycle: improved validation

--- pkg/linux/dbusx/validation.go
+++ pkg/linux/dbusx/validation.go
@@ -6,11 +6,29 @@ package dbusx
 import (
 	"errors"
 	"fmt"
+	"strings"
 
 	"github.com/go-playground/validator/v10"
 )
 
-type validationError struct {
+var (
+	// ErrValidationFailed indicates that the validation action itself failed.
+	ErrValidationFailed = errors.New("internal validation error")
+	// ErrValidation indicates that validation did not pass.
+	ErrValidation = errors.New("validation failed")
+
+	ErrInvalidField  = errors.New("invalid field")
+	ErrInvalidStruct = errors.New("invalid struct")
+)
+
+var validate *validator.Validate
+
+func init() {
+	validate = validator.New(validator.WithRequiredStructEnabled())
+}
+
+// FieldError is a particular validation error on a particular field.
+type FieldError struct {
 	Namespace       string `json:"namespace"` // can differ when a custom TagNameFunc is registered or
 	Field           string `json:"field"`     // by passing alt name to ReportError like below
 	StructNamespace string `json:"structNamespace"`
@@ -24,55 +42,62 @@ type validationError struct {
 	Message         string `json:"message"`
 }
 
-var ErrValidationError = errors.New("internal validation error")
-
-var validate *validator.Validate
-
-func init() {
-	validate = validator.New()
+// Error satisfies the Error interface.
+func (e *FieldError) Error() string {
+	return fmt.Sprintf(
+		"%s %s (value(%q)) failed validation for %s: %s",
+		ErrInvalidField.Error(),
+		e.Field,
+		e.Value,
+		e.Tag,
+		e.Message,
+	)
 }
 
-func getValidationProblems(validationErrors validator.ValidationErrors) map[string]string {
-	problems := make(map[string]string)
+// StructError contains validation errors on individual fields in a struct.
+type StructError struct {
+	Fields []FieldError
+}
 
-	for _, err := range validationErrors {
-		errInfo := validationError{
-			Namespace:       err.Namespace(),
-			Field:           err.Field(),
-			StructNamespace: err.StructNamespace(),
-			StructField:     err.StructField(),
-			Tag:             err.Tag(),
-			ActualTag:       err.ActualTag(),
-			Kind:            fmt.Sprintf("%v", err.Kind()),
-			Type:            fmt.Sprintf("%v", err.Type()),
-			Value:           fmt.Sprintf("%v", err.Value()),
-			Param:           err.Param(),
-			Message:         err.Error(),
-		}
-
-		problems[errInfo.Field] = errInfo.Message
+// Error satisfies the Error interface.
+func (e *StructError) Error() string {
+	var errStr strings.Builder
+	errStr.WriteString("contains field errors")
+	if len(e.Fields) > 0 {
+		errStr.WriteRune('\n')
 	}
-
-	// from here you can create your own error messages in whatever language you wish
-	return problems
+	for idx, t := range e.Fields {
+		errStr.WriteString(t.Error())
+		if idx < (len(e.Fields) - 1) {
+			errStr.WriteRune('\n')
+		}
+	}
+	return errStr.String()
 }
 
-//nolint:errorlint,forcetypeassert
 func valid[T any](obj *T) error {
-	err := validate.Struct(obj)
-	if err != nil {
-		switch {
-		case errors.Is(err, &validator.InvalidValidationError{}):
-			return ErrValidationError
-		case errors.Is(err, validator.ValidationErrors{}):
-			var errs error
-			for field, problem := range getValidationProblems(err.(validator.ValidationErrors)) {
-				errs = errors.Join(errs, fmt.Errorf("%s: %s", field, problem)) //nolint:err113
+	if err := validate.Struct(obj); err != nil {
+		errs := &StructError{}
+		var validateErrs validator.ValidationErrors
+		if errors.As(err, &validateErrs) {
+			errs.Fields = make([]FieldError, 0, len(validateErrs))
+			for _, err := range validateErrs {
+				errs.Fields = append(errs.Fields, FieldError{
+					Namespace:       err.Namespace(),
+					Field:           err.Field(),
+					StructNamespace: err.StructNamespace(),
+					StructField:     err.StructField(),
+					Tag:             err.Tag(),
+					ActualTag:       err.ActualTag(),
+					Kind:            fmt.Sprintf("%v", err.Kind()),
+					Type:            fmt.Sprintf("%v", err.Type()),
+					Value:           fmt.Sprintf("%v", err.Value()),
+					Param:           err.Param(),
+					Message:         err.Error(),
+				})
 			}
-
 			return errs
 		}
 	}
-
 	return nil
 }
