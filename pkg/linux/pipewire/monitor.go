863abcbd32fe67df35ba7628ed5c922fdabeafe0
Joshua Rich <joshua.rich@gmail.com>
2026-03-08 02:22:30 +0000

    fix(pkg/pipewire): :bug: handle pw-dump errors

--- pkg/linux/pipewire/monitor.go
+++ pkg/linux/pipewire/monitor.go
@@ -10,6 +10,7 @@ import (
 	"fmt"
 	"io"
 	"log/slog"
+	"os"
 	"os/exec"
 	"slices"
 	"sync"
@@ -23,9 +24,12 @@ import (
 // Monitor handles monitoring pipewire for events and dispatching events to registered listeners as appropriate.
 type Monitor struct {
 	listeners []*Listener
+	stopCh    chan struct{}
 }
 
 // NewMonitor creates a new pipewire monitor.
+//
+//nolint:gocognit
 func NewMonitor(ctx context.Context) (*Monitor, error) {
 	// Set up pw-dump command.
 	cmd := exec.CommandContext(ctx, "pw-dump", "--monitor", "--no-colors")
@@ -40,58 +44,83 @@ func NewMonitor(ctx context.Context) (*Monitor, error) {
 	// Create monitor
 	monitor := &Monitor{
 		listeners: make([]*Listener, 0),
+		stopCh:    make(chan struct{}),
 	}
 
 	// Decode pw-dump stdout as json stream.
 	dec := json.NewDecoder(stdout)
 	go func() {
 		for {
-			_, err := dec.Token()
-			if err != nil && !errors.Is(err, io.ErrClosedPipe) {
-				slogctx.FromCtx(ctx).Log(ctx, logging.LevelTrace, "pw-dump: failed to read JSON token.",
-					slog.Any("error", err))
-			}
-
-			// Read pw-dump output.
-			for dec.More() {
-				event, ok := eventPool.Get().(*Event)
-				if !ok {
-					slogctx.FromCtx(ctx).Warn("Unable to allocate event buffer.")
-					continue
-				}
-				func(e *Event) {
-					defer func() {
-						e = &Event{}
-						eventPool.Put(e)
-					}()
-					if err = dec.Decode(e); err == io.EOF {
+			select {
+			case <-monitor.stopCh:
+				return
+			default:
+				for {
+					_, err := dec.Token()
+					if err != nil && errors.Is(err, os.ErrClosed) {
+						// Stop decoding if the command stdout is closed.
 						return
-					} else if err != nil {
-						slogctx.FromCtx(ctx).Log(ctx, logging.LevelTrace, "Error decoding pw-dump output.",
+					}
+					if err != nil && !errors.Is(err, io.ErrClosedPipe) {
+						// Report any errors in decoding.
+						slogctx.FromCtx(ctx).Log(ctx, logging.LevelTrace, "pw-dump: failed to read JSON token.",
 							slog.Any("error", err))
-					} else if event == nil {
-						return
 					}
-					event.CapturedAt = time.Now().UTC()
-					// Filter the event through all listeners and send the event to whichever listeners want it.
-					for listener := range slices.Values(monitor.listeners) {
-						if listener.filterFunc(event) {
-							go func() {
-								listener.eventCh <- *event
-							}()
-						}
-					}
-				}(event)
-			}
 
-			_, err = dec.Token()
-			if err != nil && !errors.Is(err, io.ErrClosedPipe) {
-				slogctx.FromCtx(ctx).Log(ctx, logging.LevelTrace, "pw-dump: failed to read JSON token.",
-					slog.Any("error", err))
+					// Read pw-dump output.
+					for dec.More() {
+						event, ok := eventPool.Get().(*Event)
+						if !ok {
+							slogctx.FromCtx(ctx).Warn("Unable to allocate event buffer.")
+							continue
+						}
+						func(e *Event) {
+							defer func() {
+								e = &Event{}
+								eventPool.Put(e)
+							}()
+							if err = dec.Decode(e); err == io.EOF {
+								return
+							} else if err != nil {
+								slogctx.FromCtx(ctx).Log(ctx, logging.LevelTrace, "Error decoding pw-dump output.",
+									slog.Any("error", err))
+							} else if event == nil {
+								return
+							}
+							event.CapturedAt = time.Now().UTC()
+							// Filter the event through all listeners and send the event to whichever listeners want it.
+							for listener := range slices.Values(monitor.listeners) {
+								if listener.filterFunc(event) {
+									go func() {
+										listener.eventCh <- *event
+									}()
+								}
+							}
+						}(event)
+					}
+
+					_, err = dec.Token()
+					if err != nil && !errors.Is(err, io.ErrClosedPipe) {
+						// Report any errors in decoding.
+						slogctx.FromCtx(ctx).Log(ctx, logging.LevelTrace, "pw-dump: failed to read JSON token.",
+							slog.Any("error", err))
+					}
+				}
 			}
 		}
 	}()
 
+	// Wait for pw-dump to finish and log any errors.
+	go func() {
+		if err = cmd.Wait(); err != nil {
+			slogctx.FromCtx(ctx).Warn("pw-dump exited with error.",
+				slog.Any("err", err))
+		}
+		close(monitor.stopCh)
+		slogctx.FromCtx(ctx).Debug("Stopped monitoring pipewire for events.")
+	}()
+
+	// Close all monitors when we the context is cancelled.
 	go func() {
 		<-ctx.Done()
 		for listener := range slices.Values(monitor.listeners) {
