f566c8358366283e1ff388161a001700013d4d30
Joshua Rich <joshua.rich@gmail.com>
2026-03-14 02:02:25 +0000

    refactor(components/registry): :recycle: improved initialisation of registry files

--- hass/registry/util.go
+++ hass/registry/util.go
@@ -4,18 +4,20 @@
 package registry
 
 import (
+	"errors"
 	"fmt"
+	"io/fs"
 	"os"
 )
 
 func checkPath(path string) error {
-	_, err := os.Stat(path)
-	if os.IsNotExist(err) {
-		err := os.MkdirAll(path, 0o750)
-		if err != nil {
-			return fmt.Errorf("unable to create new directory: %w", err)
+	if _, err := os.Stat(path); err != nil {
+		if !errors.Is(err, fs.ErrNotExist) {
+			return fmt.Errorf("stat directory: %w", err)
+		}
+		if err := os.MkdirAll(path, 0700); err != nil {
+			return fmt.Errorf("create directory %s: %w", path, err)
 		}
 	}
-
 	return nil
 }
