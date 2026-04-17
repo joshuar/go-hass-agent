8068c2db559f625623ef47b322a3f20a7ec11a37
Joshua Rich <joshua.rich@gmail.com>
2026-04-17 23:45:37 +0000

    build(devcontainer): :arrow_up: update devcontainer

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -4,13 +4,6 @@
 
 set -x
 
-# Add starship to fish shell.
-mkdir -p ~/.config/fish
-echo "starship init fish | source" >>~/.config/fish/config.fish
-
-# Add starship to bash shell.
-echo 'eval "$(starship init bash)"' >>~/.bashrc
-
 cd /workspace
 
 # Update JS packages.
@@ -19,11 +12,11 @@ npm clean-install || exit -1
 # Install Go packages.
 export PATH="$HOME/go/bin:/go/bin:/usr/local/go/bin:$PATH" && \
     go mod tidy && \
-    go install github.com/air-verse/air@7a8024892f50b53ff20f733de9cbd662a730095b && \
-    go install github.com/a-h/templ/cmd/templ@5ddd784440b232930161d76c7ca85d922fdcf183 && \
-    go install golang.org/x/tools/gopls@5c4433be420451410e8cfd968eda32a818dac087 && \
-    go install github.com/sigstore/cosign/v3/cmd/cosign@479147a4df05f31be48aeb2b3a9d32dfc35ba877 && \
-    curl -sSfL https://golangci-lint.run/install.sh | sh -s -- -b $(go env GOPATH)/bin v2.8.0 && \
+    go install github.com/air-verse/air@latest && \
+    go install github.com/a-h/templ/cmd/templ@latest && \
+    go install golang.org/x/tools/gopls@latest && \
+    go install github.com/sigstore/cosign/v3/cmd/cosign@latest && \
+    curl -sSfL https://golangci-lint.run/install.sh | sh -s -- -b $(go env GOPATH)/bin v2.11.4 && \
     golangci-lint custom && \
     mv /tmp/golangci-lint-v2 $(go env GOPATH)/bin/golangci-lint-v2
 
